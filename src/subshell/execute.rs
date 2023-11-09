d927665ad1b663e7f11fd956810210ddc6a49aa7
Kevin Goslar <kevin.goslar@gmail.com>
2023-11-09 17:07:22 -0600

    execute.rs

--- src/subshell/execute.rs
+++ src/subshell/execute.rs
@@ -5,7 +5,7 @@ use std::process::{Command, ExitCode};
 pub fn execute(app: RunnableApp, args: Vec<String>) -> Result<ExitCode> {
     let mut cmd = Command::new(app.executable);
     cmd.args(args);
-    let status = cmd.status().unwrap();
-    let code = status.code().unwrap_or(0) as u8;
-    Ok(ExitCode::from(code))
+    let exit_status = cmd.status().unwrap();
+    let exit_code = exit_status.code().unwrap_or(0) as u8;
+    Ok(ExitCode::from(exit_code))
 }
