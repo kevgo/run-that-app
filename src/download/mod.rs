37efa190726ff0241dfe0519a3b84dae32b2ad33
Kevin Goslar <kevin.goslar@gmail.com>
2023-11-10 09:17:55 -0600

    mod.rs

--- src/download/mod.rs
+++ src/download/mod.rs
@@ -6,7 +6,7 @@ pub use http_get::http_get;
 
 /// An artifacts is a file containing an application, downloaded from the internet.
 /// An artifact could be an archive containing the application binary (and other files),
-/// or the application binary itself.
+/// or the uncompressed application binary itself.
 pub struct Artifact {
     pub filename: String,
     pub data: Vec<u8>,
