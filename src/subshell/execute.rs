5032f67b50b43a3571eac93f55ccf042965b2e10
Kevin Goslar <kevin.goslar@gmail.com>
2023-11-10 20:10:47 -0600

    execute.rs

--- src/subshell/execute.rs
+++ src/subshell/execute.rs
@@ -4,12 +4,11 @@ use std::process::{Command, ExitCode};
 pub fn execute(app: RunnableApp, args: Vec<String>) -> ExitCode {
     let mut cmd = Command::new(app.executable);
     cmd.args(args);
-    let exit_status = cmd.status().unwrap();
-    let exit_code = exit_status.code().unwrap_or(255);
-    ExitCode::from(reduce_i32_to_u8(exit_code))
+    let exit_code = cmd.status().unwrap().code().unwrap_or(255);
+    ExitCode::from(reduce_exit_status_to_code(exit_code))
 }
 
-fn reduce_i32_to_u8(code: i32) -> u8 {
+fn reduce_exit_status_to_code(code: i32) -> u8 {
     if code < 0 {
         return 255;
     }
@@ -17,4 +16,25 @@ fn reduce_i32_to_u8(code: i32) -> u8 {
         return 255;
     }
     u8::try_from(code).unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn reduce_exit_status_to_code() {
+        let tests: Vec<(i32, u8)> = vec![
+            (i32::MIN, 255),
+            (-1, 255),
+            (0, 0),
+            (1, 1),
+            (254, 254),
+            (255, 255),
+            (256, 255),
+            (i32::MAX, 255),
+        ];
+        for (give, want) in tests {
+            assert_eq!(super::reduce_exit_status_to_code(give), want);
+        }
+    }
 }
