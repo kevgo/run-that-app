3b513d8da6616954662fe98f42c28a4a3b32cc81
Kevin Goslar <kevin.goslar@gmail.com>
2023-11-13 20:57:34 -0600

    zip.rs

--- src/archives/zip.rs
+++ src/archives/zip.rs
@@ -18,14 +18,14 @@ impl Archive for Zip {
     fn extract(
         &self,
         data: Vec<u8>,
-        path_in_archive: &str,
-        path_on_disk: &Path,
+        filepath_in_archive: &str,
+        filepath_on_disk: &Path,
         output: &dyn Output,
     ) -> Result<Executable> {
         output.print("extracting zip archive ... ");
         let mut zip_archive =
             zip::ZipArchive::new(io::Cursor::new(&data)).expect("cannot read zip data");
-        if let Some(parent_dir) = path_on_disk.parent() {
+        if let Some(parent_dir) = filepath_on_disk.parent() {
             if !parent_dir.exists() {
                 std::fs::create_dir_all(parent_dir).unwrap();
             }
@@ -34,12 +34,12 @@ impl Archive for Zip {
             let file_in_zip = zip_archive.by_index(i).unwrap();
             output.log(CATEGORY, &format!("- {}", file_in_zip.name()));
         }
-        let mut file_in_zip = zip_archive.by_name(path_in_archive).unwrap();
-        let mut file_on_disk = fs::File::create(path_on_disk).unwrap();
+        let mut file_in_zip = zip_archive.by_name(filepath_in_archive).unwrap();
+        let mut file_on_disk = fs::File::create(filepath_on_disk).unwrap();
         io::copy(&mut file_in_zip, &mut file_on_disk).unwrap();
-        filesystem::make_file_executable(path_on_disk)?;
+        filesystem::make_file_executable(filepath_on_disk)?;
         output.println(&format!("{}", "ok".green()));
-        Ok(Executable(path_on_disk.to_path_buf()))
+        Ok(Executable(filepath_on_disk.to_path_buf()))
     }
 }
 
