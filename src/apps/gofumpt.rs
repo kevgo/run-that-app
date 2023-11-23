4c41bc581acd87213a561cbc88cdcfcfca4429b7
Kevin Goslar <kevin.goslar@gmail.com>
2023-11-23 10:46:45 -0600

    gofumpt.rs

--- src/apps/gofumpt.rs
+++ src/apps/gofumpt.rs
@@ -32,17 +32,25 @@ impl App for Gofumpt {
         vec![
             Box::new(DownloadPrecompiledBinary {
                 name: self.name(),
-                url: format!("https://github.com/mvdan/gofumpt/releases/download/v{version}/gofumpt_v{version}_{os}_{cpu}", os = os_text(platform.os), cpu = cpu_text(platform.cpu)),
+                url: download_url(version, platform),
                 artifact_type: ArtifactType::Executable,
-                file_on_disk: yard.app_file_path(self.name(), version, self.executable_filename(platform)),
+                file_on_disk: yard.app_file_path(
+                    self.name(),
+                    version,
+                    self.executable_filename(platform),
+                ),
             }),
             Box::new(CompileFromGoSource {
                 import_path: format!("mvdan.cc/gofumpt@{version}"),
                 target_folder: yard.app_folder(self.name(), version),
                 executable_filename: self.executable_filename(platform),
-             }),
+            }),
         ]
     }
+}
+
+fn download_url(version: &str, platform: Platform) -> String {
+    format!("https://github.com/mvdan/gofumpt/releases/download/v{version}/gofumpt_v{version}_{os}_{cpu}", os = os_text(platform.os), cpu = cpu_text(platform.cpu))
 }
 
 fn os_text(os: Os) -> &'static str {
@@ -57,5 +65,22 @@ fn cpu_text(cpu: Cpu) -> &'static str {
     match cpu {
         Cpu::Arm64 => "arm64",
         Cpu::Intel64 => "amd64",
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::detect::{Cpu, Os, Platform};
+
+    #[test]
+    fn download_url() {
+        let platform = Platform {
+            os: Os::MacOS,
+            cpu: Cpu::Arm64,
+        };
+        let have = super::download_url("0.5.0", platform);
+        let want =
+            "https://github.com/mvdan/gofumpt/releases/download/v0.5.0/gofumpt_v0.5.0_darwin_arm64";
+        assert_eq!(have, want);
     }
 }
