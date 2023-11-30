ddfebef4bfd8447cffd36f3331b859a1371a0509
Kevin Goslar <kevin.goslar@gmail.com>
2023-11-30 17:09:35 -0600

    run.rs show_path.rs

--- src/cmd/run.rs
+++ src/cmd/run.rs
@@ -1,20 +1,27 @@
 use crate::apps;
-use crate::apps::App;
 use crate::cli::RequestedApp;
 use crate::config;
 use crate::error::UserError;
 use crate::filesystem::find_global_install;
 use crate::platform;
-use crate::platform::Platform;
 use crate::subshell;
 use crate::yard;
 use crate::yard::Executable;
-use crate::yard::Yard;
 use crate::Output;
 use crate::Result;
 use std::process::ExitCode;
 
-pub fn run(mut requested_app: RequestedApp, args: Vec<String>, include_global: bool, optional: bool, output: &dyn Output) -> Result<ExitCode> {
+pub fn run(requested_app: RequestedApp, args: Vec<String>, include_global: bool, optional: bool, output: &dyn Output) -> Result<ExitCode> {
+    if let Some(executable) = load_or_install(requested_app, include_global, output)? {
+        Ok(subshell::execute(executable, args))
+    } else if optional {
+        Ok(ExitCode::SUCCESS)
+    } else {
+        Err(UserError::UnsupportedPlatform)
+    }
+}
+
+pub fn load_or_install(mut requested_app: RequestedApp, include_global: bool, output: &dyn Output) -> Result<Option<Executable>> {
     if requested_app.version.is_empty() {
         let config = config::load()?;
         let Some(configured_app) = config.lookup(&requested_app.name) else {
@@ -25,27 +32,10 @@ pub fn run(mut requested_app: RequestedApp, args: Vec<String>, include_global: b
     let app = apps::lookup(&requested_app.name)?;
     let platform = platform::detect(output)?;
     let prodyard = yard::load_or_create(&yard::production_location()?)?;
-    if let Some(executable) = load_or_install(&requested_app, app.as_ref(), platform, include_global, &prodyard, output)? {
-        Ok(subshell::execute(executable, args))
-    } else if optional {
-        Ok(ExitCode::SUCCESS)
-    } else {
-        Err(UserError::UnsupportedPlatform)
-    }
-}
-
-pub fn load_or_install(
-    requested_app: &RequestedApp,
-    app: &dyn App,
-    platform: Platform,
-    include_global: bool,
-    yard: &Yard,
-    output: &dyn Output,
-) -> Result<Option<Executable>> {
-    if let Some(executable) = yard.load_app(requested_app, app.executable_filename(platform)) {
+    if let Some(executable) = prodyard.load_app(&requested_app, app.executable_filename(platform)) {
         return Ok(Some(executable));
     };
-    if yard.is_not_installable(requested_app) {
+    if prodyard.is_not_installable(&requested_app) {
         if include_global {
             if let Some(executable) = find_global_install(app.executable_filename(platform), output) {
                 return Ok(Some(executable));
@@ -53,12 +43,12 @@ pub fn load_or_install(
         }
         return Ok(None);
     }
-    for installation_method in app.installation_methods(&requested_app.version, platform, yard) {
+    for installation_method in app.installation_methods(&requested_app.version, platform, &prodyard) {
         if let Some(executable) = installation_method.install(output)? {
             return Ok(Some(executable));
         }
     }
-    yard.mark_not_installable(requested_app)?;
+    prodyard.mark_not_installable(&requested_app)?;
     if include_global {
         if let Some(executable) = find_global_install(app.executable_filename(platform), output) {
             return Ok(Some(executable));
--- src/cmd/show_path.rs
+++ src/cmd/show_path.rs
@@ -1,27 +1,12 @@
-use crate::apps;
 use crate::cli::RequestedApp;
-use crate::config;
-use crate::error::UserError;
-use crate::platform;
-use crate::yard;
 use crate::Output;
 use crate::Result;
 use std::process::ExitCode;
 
 use super::run::load_or_install;
 
-pub fn show_path(mut requested_app: RequestedApp, include_global: bool, output: &dyn Output) -> Result<ExitCode> {
-    if requested_app.version.is_empty() {
-        let config = config::load()?;
-        let Some(configured_app) = config.lookup(&requested_app.name) else {
-            return Err(UserError::RunRequestMissingVersion);
-        };
-        requested_app.version = configured_app.version;
-    }
-    let app = apps::lookup(&requested_app.name)?;
-    let platform = platform::detect(output)?;
-    let prodyard = yard::load_or_create(&yard::production_location()?)?;
-    if let Some(executable) = load_or_install(&requested_app, app.as_ref(), platform, include_global, &prodyard, output)? {
+pub fn show_path(requested_app: RequestedApp, include_global: bool, output: &dyn Output) -> Result<ExitCode> {
+    if let Some(executable) = load_or_install(requested_app, include_global, output)? {
         println!("{}", executable.0.to_string_lossy());
     }
     Ok(ExitCode::SUCCESS)
