95352b9996ccfcb0f3a7075e4c043b94a64aae70
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-01 11:33:36 -0600

    Fix flaky test

--- src/subshell/execute.rs
+++ src/subshell/execute.rs
@@ -34,19 +34,22 @@ fn reduce_exit_status_to_code(code: i32) -> u8 {
 #[cfg(test)]
 mod tests {
     mod execute {
+        use std::io::Write;
 
         #[test]
         #[cfg(unix)]
         fn unix_success() {
-            use crate::filesystem::make_file_executable;
             use crate::subshell::execute;
             use crate::yard::Executable;
             use big_s::S;
             use std::fs;
+            use std::os::unix::fs::PermissionsExt;
             let tempdir = tempfile::tempdir().unwrap();
             let executable_path = tempdir.path().join("executable");
-            fs::write(&executable_path, b"#!/bin/sh\necho hello").unwrap();
-            make_file_executable(&executable_path).unwrap();
+            let mut file = fs::File::create(&executable_path).unwrap();
+            file.write_all(b"#!/bin/sh\necho hello").unwrap();
+            file.set_permissions(fs::Permissions::from_mode(0o744)).unwrap();
+            drop(file);
             let have = execute(Executable(executable_path), vec![]).unwrap();
             // HACK: is there a better way to compare ExitCode?
             assert_eq!(format!("{have:?}"), S("ExitCode(unix_exit_status(0))"));
