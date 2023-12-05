54e6025517cba253fb616271f0467b24c8c350b8
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-04 20:30:49 -0600

    update.rs

--- src/cmd/update.rs
+++ src/cmd/update.rs
@@ -1,10 +1,16 @@
-use crate::config;
+use crate::cli::RequestedApp;
 use crate::output::Output;
 use crate::Result;
+use crate::{config, hosting};
 use std::process::ExitCode;
 
 pub fn update(output: &dyn Output) -> Result<ExitCode> {
-    let config = config::load()?;
     output.println("updating");
+    let config = config::load()?;
+    let mut new_config: Vec<RequestedApp> = vec![];
+    for app in &config.0 {
+        output.println(&format!("updating {} ...", app.name));
+    }
+    // write new_config to disk
     Ok(ExitCode::SUCCESS)
 }
