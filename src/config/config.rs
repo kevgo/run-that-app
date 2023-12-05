cfd2a2424898962abb77029c58d0eae129939e8e
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-05 07:27:06 -0600

    Improve newtype setup

--- src/config/config.rs
+++ src/config/config.rs
@@ -1,10 +1,12 @@
 use crate::cli::RequestedApp;
 
 #[derive(Debug, Default, PartialEq)]
-pub struct Config(pub Vec<RequestedApp>);
+pub struct Config {
+    pub apps: Vec<RequestedApp>,
+}
 
 impl Config {
     pub fn lookup(self, app_name: &str) -> Option<RequestedApp> {
-        self.0.into_iter().find(|app| app.name == app_name)
+        self.apps.into_iter().find(|app| app.name == app_name)
     }
 }
