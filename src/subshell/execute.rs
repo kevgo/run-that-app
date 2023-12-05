4f9db17543eb5360d46e61d0d66ea2d5f5ce1b0e
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-05 07:14:38 -0600

    Fix flaky test attempt 2 (#68)

--- src/subshell/execute.rs
+++ src/subshell/execute.rs
@@ -41,15 +41,17 @@ mod tests {
             use crate::subshell::execute;
             use crate::yard::Executable;
             use big_s::S;
-            use std::fs;
             use std::io::Write;
             use std::os::unix::fs::PermissionsExt;
+            use std::time::Duration;
+            use std::{fs, thread};
             let tempdir = tempfile::tempdir().unwrap();
             let executable_path = tempdir.path().join("executable");
             let mut file = fs::File::create(&executable_path).unwrap();
             file.write_all(b"#!/bin/sh\necho hello").unwrap();
             file.set_permissions(fs::Permissions::from_mode(0o744)).unwrap();
             drop(file);
+            thread::sleep(Duration::from_millis(10)); // give the OS time to close the file to avoid a flaky test
             let have = execute(Executable(executable_path), vec![]).unwrap();
             // HACK: is there a better way to compare ExitCode?
             assert_eq!(format!("{have:?}"), S("ExitCode(unix_exit_status(0))"));
