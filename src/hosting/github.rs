feabb04b873dfed9ed8a7d4568dc70c89d50b31e
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-05 13:49:24 -0600

    github.rs

--- src/hosting/github.rs
+++ src/hosting/github.rs
@@ -8,7 +8,7 @@ pub fn versions(org: &str, repo: &str, amount: u8, output: &dyn Output) -> Resul
     let get = minreq::get(url)
         .with_param("per_page", amount.to_string())
         .with_header("Accept", "application/vnd.github+json")
-        .with_header("User-Agent", "run-that-app")
+        .with_header("User-Agent", format!("run-that-app-{}", env!("CARGO_PKG_VERSION")))
         .with_header("X-GitHub-Api-Version", "2022-11-28");
     let Ok(response) = get.send() else {
         output.println(&format!("{}", "not online".red()));
