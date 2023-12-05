58f1b72d5568f1edc996ca2fe7f2b34910cf3ff6
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-05 13:47:31 -0600

    github.rs

--- src/hosting/github.rs
+++ src/hosting/github.rs
@@ -5,7 +5,10 @@ use colored::Colorize;
 
 pub fn versions(org: &str, repo: &str, amount: u8, output: &dyn Output) -> Result<Vec<String>> {
     let url = format!("https://api.github.com/repos/{org}/{repo}/releases?per_page={amount}");
-    let get = minreq::get(url).with_param("per_page", amount.to_string());
+    let get = minreq::get(url)
+        .with_param("per_page", amount.to_string())
+        .with_header("Accept", "application/vnd.github+json")
+        .with_header("X-GitHub-Api-Version", "2022-11-28");
     let Ok(response) = get.send() else {
         output.println(&format!("{}", "not online".red()));
         return Err(UserError::NotOnline);
