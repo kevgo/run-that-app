0b8292940b4e63996c8e93db9e90e522cc88a4e8
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-05 07:34:33 -0600

    Refactor config (#71)

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,7 +1,7 @@
+//! functionality for the `.tool-versions` file
+
 mod config;
 mod load;
-mod parse_line;
 
 pub use config::Config;
 pub use load::{load, FILE_NAME};
-pub use parse_line::parse_line;
