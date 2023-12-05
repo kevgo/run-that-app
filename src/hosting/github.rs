300bd78b4d070e48f549b811e2fa2d530056f3ea
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-04 20:06:51 -0600

    github.rs

--- src/hosting/github.rs
+++ src/hosting/github.rs
@@ -11,10 +11,10 @@ pub struct GitHub {
 impl HostingPlatform for GitHub {
     fn versions(&self, output: &dyn Output) -> Result<Vec<String>> {
         let url = format!("https://api.github.com/repos/{org}/{repo}/releases", org = self.organization, repo = self.repo);
-        let artifact = http_get(url, output).unwrap().unwrap();
-        println!("11111111111");
-        println!("{}", &artifact.filename);
-        println!("{}", String::from_utf8_lossy(&artifact.data));
+        let Some(data) = http_get(&url, output)? else {
+            panic!("GitHub API 404");
+        };
+        println!("{}", String::from_utf8_lossy(&data));
         Ok(vec![])
     }
 }
