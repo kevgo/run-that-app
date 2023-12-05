d6c50e92260822846017662e5ca016bfba318d1f
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-05 07:29:23 -0600

    update.rs

--- src/cmd/update.rs
+++ src/cmd/update.rs
@@ -8,7 +8,7 @@ pub fn update(output: &dyn Output) -> Result<ExitCode> {
     output.println("updating");
     let config = config::load()?;
     let mut new_config: Vec<RequestedApp> = vec![];
-    for app in &config.0 {
+    for app in &config.apps {
         output.println(&format!("updating {} ...", app.name));
     }
     // write new_config to disk
