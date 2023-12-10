6abeb79205959a6b0049a052eb264a6aaa0dbf2a
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-09 20:04:15 -0600

    create_parent.rs

--- src/filesystem/create_parent.rs
+++ src/filesystem/create_parent.rs
@@ -2,6 +2,7 @@ use crate::{Result, UserError};
 use std::fs;
 use std::path::Path;
 
+/// creates the folder that will contain the file with the given path on disk
 pub fn create_parent(filepath: &Path) -> Result<()> {
     if let Some(parent) = filepath.parent() {
         fs::create_dir_all(parent).map_err(|err| UserError::CannotCreateFolder {
