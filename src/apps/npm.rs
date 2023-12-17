176d67d9d3c8b76d1e1fe7ca1a4de3ee6d7b281d
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-17 15:02:19 -0600

    npm.rs

---
+++ src/apps/npm.rs
@@ -0,0 +1,101 @@
+use super::App;
+use crate::hosting::github;
+use crate::install::packaged_executable::{self, InstallArgs};
+use crate::platform::{Cpu, Os, Platform};
+use crate::yard::{Executable, Yard};
+use crate::{Output, Result};
+
+pub struct Npm {}
+
+const ORG: &str = "nodejs";
+const REPO: &str = "node";
+
+impl App for NodeJS {
+    fn name(&self) -> &'static str {
+        "npm"
+    }
+
+    fn executable_filename(&self, platform: Platform) -> &'static str {
+        match platform.os {
+            Os::Windows => "npm.exe",
+            Os::Linux | Os::MacOS => "npm",
+        }
+    }
+
+    fn homepage(&self) -> &'static str {
+        "https://nodejs.org"
+    }
+
+    fn install(&self, version: &str, platform: Platform, yard: &Yard, output: &dyn Output) -> Result<Option<Executable>> {
+        packaged_executable::install(InstallArgs {
+            artifact_url: download_url(version, platform),
+            file_to_extract: &executable_path(version, platform, self.executable_filename(platform)),
+            filepath_on_disk: yard.app_file_path(self.name(), version, self.executable_filename(platform)),
+            output,
+        })
+    }
+
+    fn latest_version(&self, output: &dyn Output) -> Result<String> {
+        github::latest(ORG, REPO, output)
+    }
+
+    fn versions(&self, amount: u8, output: &dyn Output) -> Result<Vec<String>> {
+        github::versions(ORG, REPO, amount, output)
+    }
+}
+
+fn download_url(version: &str, platform: Platform) -> String {
+    format!(
+        "https://nodejs.org/dist/v{version}/node-v{version}-{os}-{cpu}.{ext}",
+        os = os_text(platform.os),
+        cpu = cpu_text(platform.cpu),
+        ext = ext_text(platform.os)
+    )
+}
+
+fn executable_path(version: &str, platform: Platform, filename: &str) -> String {
+    format!(
+        "node-v{version}-{os}-{cpu}/bin/{filename}",
+        cpu = cpu_text(platform.cpu),
+        os = os_text(platform.os)
+    )
+}
+
+fn os_text(os: Os) -> &'static str {
+    match os {
+        Os::Linux => "linux",
+        Os::MacOS => "darwin",
+        Os::Windows => "win",
+    }
+}
+
+fn cpu_text(cpu: Cpu) -> &'static str {
+    match cpu {
+        Cpu::Arm64 => "arm64",
+        Cpu::Intel64 => "x64",
+    }
+}
+
+fn ext_text(os: Os) -> &'static str {
+    match os {
+        Os::Linux => "tar.xz",
+        Os::MacOS => "tar.gz",
+        Os::Windows => "zip",
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::platform::{Cpu, Os, Platform};
+
+    #[test]
+    fn download_url() {
+        let platform = Platform {
+            os: Os::MacOS,
+            cpu: Cpu::Arm64,
+        };
+        let have = super::download_url("20.10.0", platform);
+        let want = "https://nodejs.org/dist/v20.10.0/node-v20.10.0-darwin-arm64.tar.gz";
+        assert_eq!(have, want);
+    }
+}
