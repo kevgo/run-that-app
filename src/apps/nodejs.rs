2f31bb39119afd65607a0fb1554d99f72cde836e
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-18 02:55:42 +0000

    Print the name of the app being installed (#114)

--- src/apps/nodejs.rs
+++ src/apps/nodejs.rs
@@ -28,6 +28,7 @@ impl App for NodeJS {
 
     fn install(&self, version: &str, platform: Platform, yard: &Yard, output: &dyn Output) -> Result<Option<Executable>> {
         archive::install(InstallArgs {
+            app_name: self.name(),
             artifact_url: download_url(version, platform),
             target_dir: yard.app_folder(self.name(), version),
             strip_prefix: &format!("node-v{version}-{os}-{cpu}/", os = os_text(platform.os), cpu = cpu_text(platform.cpu)),
--- src/apps/shellcheck.rs
+++ src/apps/shellcheck.rs
@@ -28,6 +28,7 @@ impl App for ShellCheck {
 
     fn install(&self, version: &str, platform: Platform, yard: &Yard, output: &dyn Output) -> Result<Option<Executable>> {
         packaged_executable::install(InstallArgs {
+            app_name: self.name(),
             artifact_url: download_url(version, platform),
             file_to_extract: &format!("shellcheck-v{version}/{executable}", executable = self.executable_filename(platform)),
             filepath_on_disk: yard.app_file_path(self.name(), version, self.executable_filename(platform)),
