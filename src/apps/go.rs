af628e182e6c8dcb6120d076d98cf8498751931a
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-17 21:42:07 -0600

    go.rs

--- src/apps/go.rs
+++ src/apps/go.rs
@@ -37,7 +37,7 @@ impl App for Go {
     fn latest_version(&self, _output: &dyn Output) -> Result<String> {
         Ok(S("1.21.5"))
         // TODO: parse https://go.dev/dl/?mode=json (which only has the most recent 2 versions)
-        // or clone the Go repo and look at the tags (git clone https://go.googlesource.com/go).
+        // or look at the tags on the GitHub repo
     }
 
     fn load(&self, version: &str, platform: Platform, yard: &Yard) -> Option<Executable> {
@@ -45,7 +45,7 @@ impl App for Go {
     }
 
     fn versions(&self, _amount: u8, _output: &dyn Output) -> Result<Vec<String>> {
-        // TODO: clone the Go repo and look at the tags (git clone https://go.googlesource.com/go)
+        // TODO: look at the tags on the GitHub repo
         Ok(vec![S("1.21.5")])
     }
 }
