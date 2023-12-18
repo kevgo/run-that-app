ecada1cd2a13ef4d57b9d09c3cc32cd97077cd9c
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-17 18:27:16 -0600

    strip_filepath.rs

--- src/filesystem/strip_filepath.rs
+++ src/filesystem/strip_filepath.rs
@@ -1,7 +1,3 @@
 pub fn strip_filepath<'a>(filepath: &'a str, prefix: &str) -> &'a str {
-    if !filepath.starts_with(prefix) {
-        println!("FILEPATH {filepath} DOES NOT HAVE PREFIX {prefix}");
-        return filepath;
-    }
     &filepath[prefix.len()..]
 }
