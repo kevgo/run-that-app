1a0f821460a394b1bdbf53032de01e9a1e3cf0b0
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-25 14:03:25 -0600

    Customizable number of app versions (#138)

--- src/cmd/versions.rs
+++ src/cmd/versions.rs
@@ -3,10 +3,10 @@ use crate::output::Output;
 use crate::Result;
 use std::process::ExitCode;
 
-pub fn versions(app_name: &str, output: &dyn Output) -> Result<ExitCode> {
+pub fn versions(app_name: &str, amount: u8, output: &dyn Output) -> Result<ExitCode> {
     let apps = &apps::all();
     let app = apps.lookup(app_name)?;
-    let versions = app.versions(10, output)?;
+    let versions = app.versions(amount, output)?;
     println!("{app_name} is available in these versions:");
     for version in versions {
         println!("- {version}");
