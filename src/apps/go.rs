8377d7d9b8579e475f1bcfe626368bdb4a1ee34e
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-25 11:49:46 -0600

    go.rs

--- src/apps/go.rs
+++ src/apps/go.rs
@@ -1,5 +1,5 @@
 use super::App;
-use crate::hosting::github;
+use crate::hosting::github_tags;
 use crate::install::archive::{self, InstallArgs};
 use crate::platform::{Cpu, Os, Platform};
 use crate::yard::{Executable, Yard};
@@ -39,15 +39,15 @@ impl App for Go {
     }
 
     fn latest_version(&self, output: &dyn Output) -> Result<String> {
-        github::tags::latest(ORG, REPO, output)
+        github_tags::latest(ORG, REPO, output)
     }
 
     fn load(&self, version: &str, platform: Platform, yard: &Yard) -> Option<Executable> {
         yard.load_app(self.name(), version, &self.executable_path(platform))
     }
 
-    fn versions(&self, _amount: u8, output: &dyn Output) -> Result<Vec<String>> {
-        github::tags::latest(ORG, REPO, output)
+    fn versions(&self, amount: u8, output: &dyn Output) -> Result<Vec<String>> {
+        github_tags::versions(ORG, REPO, amount, output)
     }
 }
 
