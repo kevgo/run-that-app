c1d5f896178c2b2e492ec4971a9dd9bd7d01f6cd
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-25 13:46:39 -0600

    help.rs

--- src/cmd/help.rs
+++ src/cmd/help.rs
@@ -26,6 +26,7 @@ Options:
 --update                         updates the versions in .tool-versions to the latest available
 --which                          displays the path to the installed executable of the given application
 --version, -V                    displays the version of run-that-app
+--versions                       displays the available versions of the given app
 ",
     );
 }
