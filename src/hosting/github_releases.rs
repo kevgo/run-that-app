507261f66586b0bdac4c59e15e088f9801ef7745
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-25 18:07:24 -0600

    Extract parse functions (#140)

--- src/hosting/github_releases.rs
+++ src/hosting/github_releases.rs
@@ -17,9 +17,13 @@ pub fn latest(org: &str, repo: &str, output: &dyn Output) -> Result<String> {
         return Err(UserError::NotOnline);
     };
     let response_text = response.as_str().unwrap();
-    let release: Release = json::from_str(response_text).map_err(|err| UserError::CannotParseApiResponse {
+    parse_latest_response(response_text, url)
+}
+
+fn parse_latest_response(text: &str, url: String) -> Result<String> {
+    let release: Release = json::from_str(text).map_err(|err| UserError::CannotParseApiResponse {
         reason: err.to_string(),
-        text: response_text.to_string(),
+        text: text.to_string(),
         url,
     })?;
     Ok(release.standardized_version())
@@ -38,9 +42,13 @@ pub fn versions(org: &str, repo: &str, amount: u8, output: &dyn Output) -> Resul
         return Err(UserError::NotOnline);
     };
     let response_text = response.as_str().unwrap();
-    let releases: Vec<Release> = json::from_str(response_text).map_err(|err| UserError::CannotParseApiResponse {
+    parse_versions_response(response_text, url)
+}
+
+fn parse_versions_response(text: &str, url: String) -> Result<Vec<String>> {
+    let releases: Vec<Release> = json::from_str(text).map_err(|err| UserError::CannotParseApiResponse {
         reason: err.to_string(),
-        text: response_text.to_string(),
+        text: text.to_string(),
         url,
     })?;
     let versions = releases.into_iter().map(Release::standardized_version).collect();
@@ -63,11 +71,10 @@ impl Release {
 mod tests {
     use super::Release;
     use big_s::S;
-    use miniserde::json;
 
     #[test]
     #[allow(clippy::too_many_lines)]
-    fn parse() {
+    fn parse_versions_response() {
         let response = r#"
 [
   {
@@ -532,8 +539,90 @@ mod tests {
     "mentions_count": 5
   }
 ]"#;
-        let have: Vec<Release> = json::from_str(response).unwrap();
-        let want = vec![Release { tag_name: S("v1.6.26") }];
+        let have: Vec<String> = super::parse_versions_response(response, S("url")).unwrap();
+        let want = vec!["1.6.26"];
+        assert_eq!(have, want);
+    }
+
+    #[test]
+    fn parse_latest_response() {
+        let response = r#"{
+  "url": "https://api.github.com/repos/octocat/Hello-World/releases/1",
+  "html_url": "https://github.com/octocat/Hello-World/releases/v1.0.0",
+  "assets_url": "https://api.github.com/repos/octocat/Hello-World/releases/1/assets",
+  "upload_url": "https://uploads.github.com/repos/octocat/Hello-World/releases/1/assets{?name,label}",
+  "tarball_url": "https://api.github.com/repos/octocat/Hello-World/tarball/v1.0.0",
+  "zipball_url": "https://api.github.com/repos/octocat/Hello-World/zipball/v1.0.0",
+  "discussion_url": "https://github.com/octocat/Hello-World/discussions/90",
+  "id": 1,
+  "node_id": "MDc6UmVsZWFzZTE=",
+  "tag_name": "v1.0.0",
+  "target_commitish": "master",
+  "name": "v1.0.0",
+  "body": "Description of the release",
+  "draft": false,
+  "prerelease": false,
+  "created_at": "2013-02-27T19:35:32Z",
+  "published_at": "2013-02-27T19:35:32Z",
+  "author": {
+    "login": "octocat",
+    "id": 1,
+    "node_id": "MDQ6VXNlcjE=",
+    "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/octocat",
+    "html_url": "https://github.com/octocat",
+    "followers_url": "https://api.github.com/users/octocat/followers",
+    "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+    "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+    "organizations_url": "https://api.github.com/users/octocat/orgs",
+    "repos_url": "https://api.github.com/users/octocat/repos",
+    "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/octocat/received_events",
+    "type": "User",
+    "site_admin": false
+  },
+  "assets": [
+    {
+      "url": "https://api.github.com/repos/octocat/Hello-World/releases/assets/1",
+      "browser_download_url": "https://github.com/octocat/Hello-World/releases/download/v1.0.0/example.zip",
+      "id": 1,
+      "node_id": "MDEyOlJlbGVhc2VBc3NldDE=",
+      "name": "example.zip",
+      "label": "short description",
+      "state": "uploaded",
+      "content_type": "application/zip",
+      "size": 1024,
+      "download_count": 42,
+      "created_at": "2013-02-27T19:35:32Z",
+      "updated_at": "2013-02-27T19:35:32Z",
+      "uploader": {
+        "login": "octocat",
+        "id": 1,
+        "node_id": "MDQ6VXNlcjE=",
+        "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/octocat",
+        "html_url": "https://github.com/octocat",
+        "followers_url": "https://api.github.com/users/octocat/followers",
+        "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+        "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+        "organizations_url": "https://api.github.com/users/octocat/orgs",
+        "repos_url": "https://api.github.com/users/octocat/repos",
+        "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/octocat/received_events",
+        "type": "User",
+        "site_admin": false
+      }
+    }
+  ]
+}"#;
+        let have: String = super::parse_latest_response(response, S("url")).unwrap();
+        let want = S("1.0.0");
         assert_eq!(have, want);
     }
 
