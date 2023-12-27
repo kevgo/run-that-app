e143d6d6bbfb876ceb85d08231c142e21bc73636
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-27 07:29:46 -0600

    github_releases.rs

--- src/hosting/github_releases.rs
+++ src/hosting/github_releases.rs
@@ -58,7 +58,7 @@ fn parse_versions_response(text: &str, url: String) -> Result<Vec<String>> {
     })?;
     let serde_json::Value::Array(releases) = releases else {
         return Err(UserError::CannotParseApiResponse {
-            reason: S("unknown data format"),
+            reason: S("API response does not contain a list of releases"),
             text: text.to_string(),
             url,
         });
