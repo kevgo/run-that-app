26ca491e7fbcd9312e59d5fd9adbb3990cda94e5
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-27 09:05:27 -0600

    Deprecate deadcode support (#144)

--- src/apps/deadcode.rs
+++ src/apps/deadcode.rs
@@ -34,7 +34,7 @@ impl App for Deadcode {
     }
 
     fn latest_version(&self, _output: &dyn Output) -> Result<String> {
-        // TODO: use github_tags here
+        // TODO: remove this file once deadcode is integrated into golangci-lint
         Ok(S("0.16.1"))
     }
 
