90143054b0cea638247d78c5cc8c739c7db5bdd8
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-31 20:23:54 -0600

    goda

---
+++ src/apps/goda.rs
@@ -0,0 +1,107 @@
+use super::App;
+use crate::hosting::github_releases;
+use crate::install::compile_go::{compile_go, CompileArgs};
+use crate::install::packaged_executable::{self, InstallArgs};
+use crate::platform::{Cpu, Os, Platform};
+use crate::yard::{Executable, Yard};
+use crate::{Output, Result};
+use const_format::formatcp;
+
+pub struct Goda {}
+
+const ORG: &str = "loov";
+const REPO: &str = "goda";
+
+impl App for Goda {
+    fn name(&self) -> &'static str {
+        "goda"
+    }
+
+    fn executable_filename(&self, platform: Platform) -> &'static str {
+        match platform.os {
+            Os::Linux | Os::MacOS => "goda",
+            Os::Windows => "goda.exe",
+        }
+    }
+
+    fn homepage(&self) -> &'static str {
+        formatcp!("https://github.com/{ORG}/{REPO}")
+    }
+
+    fn install(&self, version: &str, platform: Platform, yard: &Yard, output: &dyn Output) -> Result<Option<Executable>> {
+        if let Some(executable) = packaged_executable::install(InstallArgs {
+            app_name: self.name(),
+            artifact_url: download_url(version, platform),
+            file_to_extract: self.executable_filename(platform),
+            filepath_on_disk: yard.app_file_path(self.name(), version, self.executable_filename(platform)),
+            output,
+        })? {
+            return Ok(Some(executable));
+        }
+        compile_go(CompileArgs {
+            import_path: format!("github.com/{ORG}/{REPO}/cmd/actionlint@{version}"),
+            target_folder: yard.app_folder(self.name(), version),
+            executable_filename: self.executable_filename(platform),
+            output,
+        })
+    }
+
+    fn latest_version(&self, output: &dyn Output) -> Result<String> {
+        github_releases::latest(ORG, REPO, output)
+    }
+
+    fn load(&self, version: &str, platform: Platform, yard: &Yard) -> Option<Executable> {
+        yard.load_app(self.name(), version, self.executable_filename(platform))
+    }
+
+    fn versions(&self, amount: usize, output: &dyn Output) -> Result<Vec<String>> {
+        github_releases::versions(ORG, REPO, amount, output)
+    }
+}
+
+fn download_url(version: &str, platform: Platform) -> String {
+    format!(
+        "https://github.com/{ORG}/{REPO}/releases/download/v{version}/actionlint_{version}_{os}_{cpu}.{ext}",
+        os = os_text(platform.os),
+        cpu = cpu_text(platform.cpu),
+        ext = ext_text(platform.os)
+    )
+}
+
+fn os_text(os: Os) -> &'static str {
+    match os {
+        Os::Linux => "linux",
+        Os::MacOS => "darwin",
+        Os::Windows => "windows",
+    }
+}
+
+fn cpu_text(cpu: Cpu) -> &'static str {
+    match cpu {
+        Cpu::Arm64 => "arm64",
+        Cpu::Intel64 => "amd64",
+    }
+}
+
+fn ext_text(os: Os) -> &'static str {
+    match os {
+        Os::Linux | Os::MacOS => "tar.gz",
+        Os::Windows => "zip",
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::platform::{Cpu, Os, Platform};
+
+    #[test]
+    fn download_url() {
+        let platform = Platform {
+            os: Os::Linux,
+            cpu: Cpu::Arm64,
+        };
+        let have = super::download_url("1.6.26", platform);
+        let want = "https://github.com/rhysd/actionlint/releases/download/v1.6.26/actionlint_1.6.26_linux_arm64.tar.gz";
+        assert_eq!(have, want);
+    }
+}
