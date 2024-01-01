c861198d30f9aacded0b522871c2d379211037a4
Kevin Goslar <kevin.goslar@gmail.com>
2023-12-31 20:14:21 -0600

    npx app (#147)

---
+++ src/apps/npx.rs
@@ -0,0 +1,43 @@
+use super::nodejs::NodeJS;
+use super::App;
+use crate::platform::{Os, Platform};
+use crate::yard::{Executable, Yard};
+use crate::{Output, Result};
+
+pub struct Npx {}
+
+impl App for Npx {
+    fn name(&self) -> &'static str {
+        "npx"
+    }
+
+    fn executable_filename(&self, platform: Platform) -> &'static str {
+        match platform.os {
+            Os::Windows => "bin\\npx.exe",
+            Os::Linux | Os::MacOS => "bin/npx",
+        }
+    }
+
+    fn homepage(&self) -> &'static str {
+        "https://www.npmjs.com"
+    }
+
+    fn install(&self, version: &str, platform: Platform, yard: &Yard, output: &dyn Output) -> Result<Option<Executable>> {
+        let nodejs = NodeJS {};
+        nodejs.install(version, platform, yard, output)?;
+        let executable_path = yard.app_file_path(nodejs.name(), version, self.executable_filename(platform));
+        Ok(Some(Executable(executable_path)))
+    }
+
+    fn latest_version(&self, output: &dyn Output) -> Result<String> {
+        (NodeJS {}).latest_version(output)
+    }
+
+    fn load(&self, version: &str, platform: Platform, yard: &Yard) -> Option<Executable> {
+        yard.load_app((NodeJS {}).name(), version, self.executable_filename(platform))
+    }
+
+    fn versions(&self, amount: usize, output: &dyn Output) -> Result<Vec<String>> {
+        (NodeJS {}).versions(amount, output)
+    }
+}
