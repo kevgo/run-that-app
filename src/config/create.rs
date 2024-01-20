bbbf0520d304c9198a090be8390057b5acd92fa9
Kevin Goslar <kevin.goslar@gmail.com>
2024-01-20 15:29:41 -0600

    mod.rs create.rs

--- src/config/create.rs
+++ src/config/create.rs
@@ -8,10 +8,12 @@ use std::io::Write;
 pub fn create() -> Result<()> {
     let mut file = match OpenOptions::new().write(true).create_new(true).open(FILE_NAME) {
         Ok(file) => file,
-        Err(err) => match err.kind() {
-            ErrorKind::AlreadyExists => return Err(UserError::ConfigFileAlreadyExists),
-            _ => panic!("{}", err),
-        },
+        Err(err) => {
+            if err.kind() == ErrorKind::AlreadyExists {
+                return Err(UserError::ConfigFileAlreadyExists);
+            }
+            panic!("{}", err);
+        }
     };
     file.write_all(CONFIG_TEXT.as_bytes())
         .map_err(|err| UserError::CannotAccessConfigFile(err.to_string()))
