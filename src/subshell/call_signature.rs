fe3c25559f22579606e00c37ae6b75af46e67a3a
Kevin Goslar <kevin.goslar@gmail.com>
2024-01-20 19:06:55 -0600

    call_signature.rs

--- src/subshell/call_signature.rs
+++ src/subshell/call_signature.rs
@@ -1,5 +1,6 @@
 use super::Executable;
 
+/// provides a printable version of the given executable invocation
 pub fn call_signature(executable: &Executable, args: &[String]) -> String {
     format!("{} {}", executable.0.to_string_lossy(), args.join(" "))
 }
