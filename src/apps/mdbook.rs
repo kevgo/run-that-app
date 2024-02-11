405aa13ca8d9a8220f328e80c5e12ae35dd3ea40
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-10 20:49:58 -0600

    MdBook (#173)

---
+++ src/apps/mdbook.rs
@@ -0,0 +1,100 @@
+use super::App;
+use crate::hosting::github_releases;
+use crate::install::compile_rust::{compile_rust, CompileArgs};
+use crate::install::packaged_executable::{self, InstallArgs};
+use crate::platform::{Cpu, Os, Platform};
+use crate::subshell::Executable;
+use crate::yard::Yard;
+use crate::{Output, Result};
+use const_format::formatcp;
+
+pub struct MdBook {}
+
+const ORG: &str = "rust-lang";
+const REPO: &str = "mdBook";
+
+impl App for MdBook {
+    fn name(&self) -> &'static str {
+        "mdBook"
+    }
+
+    fn executable_filename(&self, platform: Platform) -> &'static str {
+        match platform.os {
+            Os::Linux | Os::MacOS => "mdbook",
+            Os::Windows => "mdbook.exe",
+        }
+    }
+
+    fn homepage(&self) -> &'static str {
+        formatcp!("https://github.com/{ORG}/{REPO}")
+    }
+
+    fn install(&self, version: &str, platform: Platform, yard: &Yard, output: &dyn Output) -> Result<Option<Executable>> {
+        if let Some(executable) = packaged_executable::install(InstallArgs {
+            app_name: self.name(),
+            artifact_url: download_url(version, platform),
+            file_to_extract: self.executable_filename(platform),
+            filepath_on_disk: yard.app_file_path(self.name(), version, self.executable_filename(platform)),
+            output,
+        })? {
+            return Ok(Some(executable));
+        }
+        compile_rust(CompileArgs {
+            crate_name: "mdbook",
+            target_folder: yard.app_folder(self.name(), version),
+            executable_filename: self.executable_filename(platform),
+            output,
+        })
+    }
+
+    fn latest_installable_version(&self, output: &dyn Output) -> Result<String> {
+        github_releases::latest(ORG, REPO, output)
+    }
+
+    fn load(&self, version: &str, platform: Platform, yard: &Yard) -> Option<Executable> {
+        yard.load_app(self.name(), version, self.executable_filename(platform))
+    }
+
+    fn installable_versions(&self, amount: usize, output: &dyn Output) -> Result<Vec<String>> {
+        github_releases::versions(ORG, REPO, amount, output)
+    }
+}
+
+fn download_url(version: &str, platform: Platform) -> String {
+    format!(
+        "https://github.com/{ORG}/{REPO}/releases/download/v{version}/mdbook-v{version}-{cpu}-{os}.tar.gz",
+        os = os_text(platform.os),
+        cpu = cpu_text(platform.cpu)
+    )
+}
+
+fn os_text(os: Os) -> &'static str {
+    match os {
+        Os::Linux => "unknown-linux-gnu",
+        Os::MacOS => "apple-darwin",
+        Os::Windows => "pc-windows-msvc",
+    }
+}
+
+fn cpu_text(cpu: Cpu) -> &'static str {
+    match cpu {
+        Cpu::Arm64 => "aarch64",
+        Cpu::Intel64 => "x86_64",
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::platform::{Cpu, Os, Platform};
+
+    #[test]
+    fn linux_intel() {
+        let platform = Platform {
+            os: Os::Linux,
+            cpu: Cpu::Intel64,
+        };
+        let have = super::download_url("0.4.37", platform);
+        let want = "https://github.com/rust-lang/mdBook/releases/download/v0.4.37/mdbook-v0.4.37-x86_64-unknown-linux-gnu.tar.gz";
+        assert_eq!(have, want);
+    }
+}
