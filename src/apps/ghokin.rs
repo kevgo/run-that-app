abc4c0f683f814c40317219d98bc0fadf5548f21
Kevin Goslar <kevin.goslar@gmail.com>
2024-02-29 17:10:05 -0600

    ghokin.rs gofumpt.rs goreleaser.rs

--- src/apps/ghokin.rs
+++ src/apps/ghokin.rs
@@ -30,14 +30,15 @@ impl App for Ghokin {
     }
 
     fn install(&self, version: &str, platform: Platform, yard: &Yard, output: &dyn Output) -> Result<Option<Executable>> {
-        if let Some(executable) = packaged_executable::install(InstallArgs {
+        let result = packaged_executable::install(InstallArgs {
             app_name: self.name(),
             artifact_url: download_url(version, platform),
             file_to_extract: self.executable_filename(platform),
             filepath_on_disk: yard.app_folder(self.name(), version).join(self.executable_filename(platform)),
             output,
-        })? {
-            return Ok(Some(executable));
+        })?;
+        if result.is_some() {
+            return Ok(result);
         }
         compile_go(CompileArgs {
             import_path: format!("github.com/{ORG}/{REPO}/v3@v{version}"),
