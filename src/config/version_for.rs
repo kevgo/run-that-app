c923fe90006ff366f9036a06c91afcf05264ede8
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-04 17:43:58 -0600

    version_for.rs

--- src/config/version_for.rs
+++ src/config/version_for.rs
@@ -2,7 +2,8 @@ use super::{AppName, Config, Version, Versions};
 use crate::error::UserError;
 use crate::Result;
 
-/// provides
+/// provides the version to use: if the user provided a version to use via CLI, use it.
+/// Otherwise provide the versions from the config file.
 pub fn versions_for(app: &AppName, cli_version: Option<Version>) -> Result<Versions> {
     if let Some(version) = cli_version {
         return Ok(Versions::from(version));
