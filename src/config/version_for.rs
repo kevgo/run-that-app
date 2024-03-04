6212ec4f3d0ef40c48c7fe109986eb544e5f7cff
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-04 17:42:32 -0600

    version_for.rs

--- src/config/version_for.rs
+++ src/config/version_for.rs
@@ -2,13 +2,14 @@ use super::{AppName, Config, Version, Versions};
 use crate::error::UserError;
 use crate::Result;
 
+/// provides
 pub fn versions_for(app: &AppName, cli_version: Option<Version>) -> Result<Versions> {
     if let Some(version) = cli_version {
         return Ok(Versions::from(version));
     }
     let config = Config::load()?;
-    let Some(versions) = config.lookup(app) else {
-        return Err(UserError::RunRequestMissingVersion);
-    };
-    Ok(versions)
+    match config.lookup(app) {
+        Some(versions) => Ok(versions),
+        None => Err(UserError::RunRequestMissingVersion),
+    }
 }
