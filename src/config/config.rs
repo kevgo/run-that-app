416857024355cf618b1eec76722a887d9d46123e
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-04 06:15:56 -0600

    config.rs

--- src/config/config.rs
+++ src/config/config.rs
@@ -15,8 +15,8 @@ impl Config {
 
 impl Display for Config {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for AppVersion { app: name, version } in &self.apps {
-            f.write_fmt(format_args!("{name} {version}\n"))?;
+        for AppVersion { app, version } in &self.apps {
+            f.write_fmt(format_args!("{app} {version}\n"))?;
         }
         Ok(())
     }
