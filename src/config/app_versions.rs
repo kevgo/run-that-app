fb7726757153e9de1a1d0b7169ee480cf33c55a8
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-04 16:13:13 -0600

    app_versions.rs

--- src/config/app_versions.rs
+++ src/config/app_versions.rs
@@ -1,17 +1,7 @@
 use super::{AppName, Versions};
-use crate::cli::AppVersion;
 
 #[derive(Debug, PartialEq)]
 pub struct AppVersions {
     pub app: AppName,
     pub versions: Versions,
-}
-
-impl From<AppVersion> for AppVersions {
-    fn from(app_version: AppVersion) -> Self {
-        AppVersions {
-            app: app_version.app,
-            versions: Versions::from(app_version.version),
-        }
-    }
 }
