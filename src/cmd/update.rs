fbee3432c6639f90cc2af91fc40b0e2d36c8ff59
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-04 15:25:12 -0600

    update.rs

--- src/cmd/update.rs
+++ src/cmd/update.rs
@@ -6,14 +6,15 @@ use colored::Colorize;
 use std::process::ExitCode;
 
 pub fn update(output: &dyn Output) -> Result<ExitCode> {
-    let old_config = Config::load()?;
+    let mut old_config = Config::load()?;
     let all_apps = apps::all();
-    for old_app in old_config.apps {
+    for old_app in &mut old_config.apps {
         let app = all_apps.lookup(&old_app.app)?;
         output.print(&format!("updating {} ... ", &old_app.app));
         let latest = app.latest_installable_version(output)?;
-        if let Some(previous) = old_app.versions.update_largest_with(latest) {
-            output.println(&format!("{} -> {}", previous.as_str().cyan(), latest.as_str().cyan()));
+        let latest_str = latest.to_string();
+        if let Some(previous) = &old_app.versions.update_largest_with(latest) {
+            output.println(&format!("{} -> {}", previous.as_str().cyan(), latest_str.cyan()));
         } else {
             output.println(&format!("{}", "current".green()));
         }
