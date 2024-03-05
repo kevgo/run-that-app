999a6ff0f6c05870c0196dfff6297689c3ebf625
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-05 06:13:13 -0600

    Extract Output into its own file

---
+++ src/output/output.rs
@@ -0,0 +1,13 @@
+pub trait Output {
+    /// indicates whether the given category is active
+    fn is_active(&self, category: &str) -> bool;
+
+    /// this output only gets displayed if the respective logging category is enabled
+    fn log(&self, category: &str, text: &str);
+
+    /// this output always gets displayed
+    fn print(&self, text: &str);
+
+    /// this output always gets displayed
+    fn println(&self, text: &str);
+}
