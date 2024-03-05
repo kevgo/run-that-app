81cdbb85d21d06397a0b6350b29eec1c7dcfcc67
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-05 06:21:01 -0600

    Extract Output into its own file (#215)

--- src/output/mod.rs
+++ src/output/mod.rs
@@ -1,19 +1,7 @@
 //! Output to the user
 
 mod console;
+mod output;
 
 pub use console::StdErr;
-
-pub trait Output {
-    /// indicates whether the given category is active
-    fn is_active(&self, category: &str) -> bool;
-
-    /// this output only gets displayed if the respective logging category is enabled
-    fn log(&self, category: &str, text: &str);
-
-    /// this output always gets displayed
-    fn print(&self, text: &str);
-
-    /// this output always gets displayed
-    fn println(&self, text: &str);
-}
+pub use output::Output;
