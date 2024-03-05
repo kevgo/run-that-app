9ad97115358850ad4311ffb74a0b611dfaf1de83
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-04 20:40:08 -0600

    mod.rs version_for.rs versions.rs main.rs

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -4,14 +4,12 @@ mod app_name;
 mod app_versions;
 mod config;
 mod version;
-mod version_for;
 mod versions;
 
 pub use app_name::AppName;
 pub use app_versions::AppVersions;
 pub use config::Config;
 pub use version::Version;
-pub use version_for::versions_for;
 pub use versions::Versions;
 
 pub const FILE_NAME: &str = ".tool-versions";
--- src/config/versions.rs
+++ src/config/versions.rs
@@ -1,10 +1,24 @@
-use super::Version;
+use super::{AppName, Config, Version};
+use crate::error::UserError;
+use crate::Result;
 
 /// a collection of Version instances
 #[derive(Debug, PartialEq)]
 pub struct Versions(Vec<Version>);
 
 impl Versions {
+    /// provides the version to use: if the user provided a version to use via CLI, use it.
+    /// Otherwise provide the versions from the config file.
+    pub fn expand(app: &AppName, cli_version: Option<Version>) -> Result<Versions> {
+        if let Some(version) = cli_version {
+            return Ok(Versions::from(version));
+        }
+        match Config::load()?.lookup(app) {
+            Some(versions) => Ok(versions),
+            None => Err(UserError::RunRequestMissingVersion),
+        }
+    }
+
     pub fn iter(&self) -> std::slice::Iter<'_, Version> {
         self.0.iter()
     }
