0bb5c700c3a893190de120600f1583d51eb878ff
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-07 14:56:41 -0600

    which.rs

--- src/cmd/which.rs
+++ src/cmd/which.rs
@@ -1,6 +1,5 @@
 use crate::apps;
-use crate::config::AppName;
-use crate::config::RequestedVersions;
+use crate::config::{AppName, RequestedVersions};
 use crate::platform;
 use crate::Output;
 use crate::Result;
@@ -9,8 +8,8 @@ use std::process::ExitCode;
 use super::run::load_or_install;
 
 pub fn which(app: &AppName, versions: &RequestedVersions, output: &dyn Output) -> Result<ExitCode> {
-    let apps = apps::all();
-    let app = apps.lookup(app)?;
+    let binding = apps::all();
+    let app = binding.lookup(app)?;
     let platform = platform::detect(output)?;
     for version in versions.iter() {
         if let Some(executable) = load_or_install(app, version, platform, output)? {
