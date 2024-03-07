8c4decc7f484ed03c483b7fcebc3889c6cf24b61
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-07 09:25:52 -0600

    Extract read_file function (#223)

---
+++ src/filesystem/read_file.rs
@@ -0,0 +1,23 @@
+use crate::error::UserError;
+use crate::Result;
+use std::{env, fs, io};
+
+/// looks for the file with the given name in the current or parent folders, and provides its content if it finds one
+pub fn read_file(name: &str) -> Result<Option<String>> {
+    let cwd = env::current_dir().map_err(|err| UserError::CannotDetermineCurrentDirectory(err.to_string()))?;
+    let mut dir = cwd.as_path();
+    loop {
+        let file_path = dir.join(name);
+        match fs::read_to_string(file_path) {
+            Ok(text) => return Ok(Some(text)),
+            Err(err) => match err.kind() {
+                io::ErrorKind::NotFound => {}
+                _ => return Err(UserError::CannotAccessConfigFile(err.to_string())),
+            },
+        }
+        dir = match dir.parent() {
+            Some(parent) => parent,
+            None => return Ok(None),
+        };
+    }
+}
