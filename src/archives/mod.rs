7d21d60d6f68ae71bb1ded9787268c70f2dd1b4b
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-08 14:03:53 -0600

    mod.rs tar_gz.rs tar_xz.rs zip.rs archive.rs

--- src/archives/mod.rs
+++ src/archives/mod.rs
@@ -2,54 +2,31 @@ mod tar_gz;
 mod tar_xz;
 mod zip;
 
-use crate::download::Artifact;
 use crate::subshell::Executable;
-use crate::{Output, Result, UserError};
+use crate::{Output, Result};
 use std::path::Path;
+
+use self::tar_gz::TarGz;
+use self::tar_xz::TarXz;
+use self::zip::Zip;
 
 /// An archive is a compressed file containing an application.
 pub trait Archive {
-    /// indicates whether this archive implementation can extract the file with the given name
-    fn can_extract(&self, filename: &str) -> bool;
-
     /// extracts all files from the given archive data to the given location on disk
-    fn extract_all(&self, data: Vec<u8>, target_dir: &Path, strip_prefix: &str, executable_path_in_archive: &str, output: &dyn Output) -> Result<Executable>;
+    fn extract_all(&self, target_dir: &Path, strip_prefix: &str, executable_path_in_archive: &str, output: &dyn Output) -> Result<Executable>;
 
     /// extracts the given file from the given archive data to the given location on disk
-    fn extract_file(&self, data: Vec<u8>, filepath_in_archive: &str, folder_on_disk: &Path, output: &dyn Output) -> Result<Executable>;
-}
-
-/// extracts all files from the given artifact data to the given folder on disk
-pub fn extract_all(args: ExtractAllArgs) -> Result<Executable> {
-    let Some(archive) = lookup(&args.artifact.filename) else {
-        return Err(UserError::UnknownArchive(args.artifact.filename));
-    };
-    archive.extract_all(args.artifact.data, args.dir_on_disk, args.strip_prefix, args.executable_in_archive, args.output)
-}
-
-pub struct ExtractAllArgs<'a> {
-    pub artifact: Artifact,
-    pub dir_on_disk: &'a Path,
-    pub strip_prefix: &'a str,
-    pub executable_in_archive: &'a str,
-    pub output: &'a dyn Output,
-}
-
-/// extracts the given file from the given artifact data to the given folder on disk
-pub fn extract_file(artifact: Artifact, path_in_archive: &str, filepath_on_disk: &Path, output: &dyn Output) -> Result<Executable> {
-    let Some(archive) = lookup(&artifact.filename) else {
-        return Err(UserError::UnknownArchive(artifact.filename));
-    };
-    archive.extract_file(artifact.data, path_in_archive, filepath_on_disk, output)
-}
-
-fn all_archives() -> Vec<Box<dyn Archive>> {
-    vec![Box::new(tar_gz::TarGz {}), Box::new(tar_xz::TarXz {}), Box::new(zip::Zip {})]
+    fn extract_file(&self, filepath_in_archive: &str, folder_on_disk: &Path, output: &dyn Output) -> Result<Executable>;
 }
 
 /// provides the archive that can extract the given file path
-fn lookup(filepath: &str) -> Option<Box<dyn Archive>> {
-    all_archives().into_iter().find(|archive| archive.can_extract(filepath))
+pub fn lookup(filepath: &str, data: Vec<u8>) -> Option<Box<dyn Archive>> {
+    match () {
+        () if filepath.ends_with(".zip") => Some(Box::new(Zip { data })),
+        () if filepath.ends_with(".tar.gz") => Some(Box::new(TarGz { data })),
+        () if filepath.ends_with(".tar.xz") => Some(Box::new(TarXz { data })),
+        () => None,
+    }
 }
 
 fn print_header(category: &str, archive_type: &str, output: &dyn Output) {
@@ -74,13 +51,13 @@ mod tests {
 
         #[test]
         fn known_archive_type() {
-            let have = lookup(".zip").unwrap();
-            assert!(have.can_extract(".zip"));
+            let have = lookup("archive.zip", vec![]);
+            assert!(have.is_some());
         }
 
         #[test]
         fn unknown_archive_type() {
-            let have = lookup(".zonk");
+            let have = lookup(".zonk", vec![]);
             assert!(have.is_none());
         }
     }
