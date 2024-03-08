5aab236d87bcec91f21864250d49359908fab0f0
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-08 15:21:45 -0600

    has_extension.rs

--- src/filesystem/has_extension.rs
+++ src/filesystem/has_extension.rs
@@ -1,7 +1,6 @@
 pub fn has_extension(filename: &str, extension: &str) -> bool {
     let filename_len = filename.len();
-    let ext_len = extension.len();
-    filename[filename_len - ext_len..filename_len].eq_ignore_ascii_case(extension)
+    filename[filename_len - extension.len()..filename_len].eq_ignore_ascii_case(extension)
 }
 
 #[cfg(test)]
