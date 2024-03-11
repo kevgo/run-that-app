b9c7963d6cf492cc4400f94c43b88492c623c970
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-11 11:50:09 -0500

    other_app_folder.rs

--- src/install/other_app_folder.rs
+++ src/install/other_app_folder.rs
@@ -10,9 +10,9 @@ pub trait OtherAppFolder: App {
 }
 
 pub fn install_other_app(app: &dyn OtherAppFolder, version: &Version, platform: Platform, output: &dyn Output) -> Result<bool> {
-    let app_name = app.app_to_install();
+    let app_to_install = app.app_to_install();
     let all_apps = apps::all();
-    let app = all_apps.lookup(&app.name())?;
+    let app = all_apps.lookup(&app_to_install.name())?;
     // Note: we know it must be the Yard variant here. At this point we are installing the app.
     // Only Yard variants get installed. The Path variant doesn't get installed.
     load_or_install(app, RequestedVersion::Yard(version.to_owned()), platform, output)?;
