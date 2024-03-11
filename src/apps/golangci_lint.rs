69b1ed1bbaac5f2e54a53887a550c000915554b6
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-11 10:51:54 -0500

    golangci_lint.rs

--- src/apps/golangci_lint.rs
+++ src/apps/golangci_lint.rs
@@ -1,11 +1,10 @@
 use super::{AnalyzeResult, App};
 use crate::config::{AppName, Version};
 use crate::hosting::github_releases;
-use crate::install::archive::{self, InstallArgs};
+use crate::install::Method;
 use crate::platform::{Cpu, Os, Platform};
-use crate::regexp;
 use crate::subshell::Executable;
-use crate::yard::Yard;
+use crate::{install, regexp};
 use crate::{Output, Result};
 use const_format::formatcp;
 
@@ -23,25 +22,13 @@ impl App for GolangCiLint {
         formatcp!("https://github.com/{ORG}/{REPO}")
     }
 
-    fn install(&self, version: &Version, platform: Platform, yard: &Yard, output: &dyn Output) -> Result<Option<Executable>> {
-        let name = self.name();
-        archive::install(InstallArgs {
-            app_name: &name,
-            artifact_url: download_url(version, platform),
-            output,
-            dir_on_disk: yard.app_folder(&name, version),
-            strip_path_prefix: &format!("golangci-lint-{version}-{os}-{cpu}/", os = os_text(platform.os), cpu = cpu_text(platform.cpu)),
-            executable_in_archive: &self.executable_filepath(platform),
-        })
+    fn install_methods(&self) -> Vec<install::Method> {
         // install from source not recommended, see https://golangci-lint.run/usage/install/#install-from-source
+        vec![Method::DownloadArchive(self)]
     }
 
     fn latest_installable_version(&self, output: &dyn Output) -> Result<Version> {
         github_releases::latest(ORG, REPO, output)
-    }
-
-    fn load(&self, version: &Version, platform: Platform, yard: &Yard) -> Option<Executable> {
-        yard.load_app(&self.name(), version, &self.executable_filepath(platform))
     }
 
     fn installable_versions(&self, amount: usize, output: &dyn Output) -> Result<Vec<Version>> {
@@ -56,26 +43,22 @@ impl App for GolangCiLint {
     }
 }
 
-fn cpu_text(cpu: Cpu) -> &'static str {
-    match cpu {
-        Cpu::Arm64 => "arm64",
-        Cpu::Intel64 => "amd64",
-    }
-}
-
-fn download_url(version: &Version, platform: Platform) -> String {
-    format!(
-        "https://github.com/{ORG}/{REPO}/releases/download/v{version}/golangci-lint-{version}-{os}-{cpu}.{ext}",
-        os = os_text(platform.os),
-        cpu = cpu_text(platform.cpu),
-        ext = ext_text(platform.os)
-    )
-}
-
-fn ext_text(os: Os) -> &'static str {
-    match os {
-        Os::Linux | Os::MacOS => "tar.gz",
-        Os::Windows => "zip",
+impl install::InstallByArchive for GolangCiLint {
+    fn archive_url(&self, version: &Version, platform: Platform) -> String {
+        let os = match platform.os {
+            Os::Linux => "linux",
+            Os::MacOS => "darwin",
+            Os::Windows => "windows",
+        };
+        let cpu = match platform.cpu {
+            Cpu::Arm64 => "arm64",
+            Cpu::Intel64 => "amd64",
+        };
+        let ext = match platform.os {
+            Os::Linux | Os::MacOS => "tar.gz",
+            Os::Windows => "zip",
+        };
+        format!("https://github.com/{ORG}/{REPO}/releases/download/v{version}/golangci-lint-{version}-{os}-{cpu}.{ext}")
     }
 }
 
@@ -83,23 +66,17 @@ fn extract_version(output: &str) -> Option<&str> {
     regexp::first_capture(output, r"golangci-lint has version (\d+\.\d+\.\d+) built with")
 }
 
-fn os_text(os: Os) -> &'static str {
-    match os {
-        Os::Linux => "linux",
-        Os::MacOS => "darwin",
-        Os::Windows => "windows",
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use crate::config::Version;
+    use crate::install::InstallByArchive;
     use crate::platform::{Cpu, Os, Platform};
 
     #[test]
-    fn download_url() {
+    fn archive_url() {
+        let golangci_lint = super::GolangCiLint {};
         let platform = Platform { os: Os::MacOS, cpu: Cpu::Arm64 };
-        let have = super::download_url(&Version::from("1.55.2"), platform);
+        let have = golangci_lint.archive_url(&Version::from("1.55.2"), platform);
         let want = "https://github.com/golangci/golangci-lint/releases/download/v1.55.2/golangci-lint-1.55.2-darwin-arm64.tar.gz";
         assert_eq!(have, want);
     }
