63537d758ced0913c87deb97f0e085e2db621533
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-11 10:12:36 -0500

    gofmt.rs other_app_folder.rs

--- src/apps/gofmt.rs
+++ src/apps/gofmt.rs
@@ -1,7 +1,7 @@
 use super::go::Go;
 use super::{AnalyzeResult, App};
 use crate::config::{AppName, Version};
-use crate::install::Method;
+use crate::install::{Method, OtherAppFolder};
 use crate::platform::Platform;
 use crate::subshell::Executable;
 use crate::{install, Output, Result};
@@ -22,23 +22,16 @@ impl App for Gofmt {
         "https://go.dev"
     }
 
-    // fn install(&self, version: &Version, platform: Platform, yard: &Yard, output: &dyn Output) -> Result<Option<Executable>> {
-    //     let go = Go {};
-    //     go.install(version, platform, yard, output)?;
-    //     let executable_path = yard.create_app_folder(&go.name(), version)?.join(self.executable_filepath(platform));
-    //     Ok(Some(Executable(executable_path)))
-    // }
-
     fn install_methods(&self) -> Vec<install::Method> {
         vec![Method::InstallAnotherApp(self)]
     }
 
     fn latest_installable_version(&self, output: &dyn Output) -> Result<Version> {
-        (Go {}).latest_installable_version(output)
+        self.app_to_install().latest_installable_version(output)
     }
 
     fn installable_versions(&self, amount: usize, output: &dyn Output) -> Result<Vec<Version>> {
-        (Go {}).installable_versions(amount, output)
+        self.app_to_install().installable_versions(amount, output)
     }
 
     fn analyze_executable(&self, executable: &Executable) -> AnalyzeResult {
@@ -50,7 +43,11 @@ impl App for Gofmt {
     }
 }
 
-impl install::OtherAppFolder for Gofmt {}
+impl install::OtherAppFolder for Gofmt {
+    fn app_to_install(&self) -> Box<dyn App> {
+        Box::new(Go {})
+    }
+}
 
 fn identify(output: &str) -> bool {
     output.contains("report all errors (not just the first 10 on different lines)")
--- src/install/other_app_folder.rs
+++ src/install/other_app_folder.rs
@@ -1,20 +1,18 @@
+use crate::apps::App;
 use crate::cmd::run::load_or_install;
-use crate::config::{AppName, RequestedVersion, Version};
+use crate::config::{RequestedVersion, Version};
 use crate::output::Output;
 use crate::platform::Platform;
 use crate::{apps, Result};
 
-pub trait OtherAppFolder {
-    fn app_to_install(&self) -> AppName {
-        // TODO: return Box::new(Go {}) here to link to the type directly and avoid stringly-typed code
-        AppName::from("Go")
-    }
+pub trait OtherAppFolder: App {
+    fn app_to_install(&self) -> Box<dyn App>;
 }
 
 pub fn install_other_app(app: &dyn OtherAppFolder, version: Version, platform: Platform, output: &dyn Output) -> Result<bool> {
     let app_name = app.app_to_install();
     let all_apps = apps::all();
-    let app = all_apps.lookup(&app_name)?;
+    let app = all_apps.lookup(&app.name())?;
     // Note: we know it must be the Yard variant here. At this point we are installing the app.
     // Only Yard variants get installed. The Path variant doesn't get installed.
     load_or_install(app, RequestedVersion::Yard(version), platform, output)?;
