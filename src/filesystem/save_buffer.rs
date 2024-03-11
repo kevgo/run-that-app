489125be9fb31c8b398cae586f75ae5bd88fcb47
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-11 12:03:01 -0500

    save_buffer.rs

--- src/filesystem/save_buffer.rs
+++ src/filesystem/save_buffer.rs
@@ -1,3 +1,4 @@
+use crate::error::UserError;
 use crate::output::Output;
 use crate::subshell::Executable;
 use crate::Result;
@@ -7,6 +8,12 @@ use std::path::Path;
 /// saves the given file data as an executable file
 pub fn save_executable(data: Vec<u8>, path_on_disk: &Path, output: &dyn Output) -> Result<Executable> {
     output.print("saving ... ");
+    if let Some(parent) = path_on_disk.parent() {
+        fs::create_dir_all(parent).map_err(|err| UserError::CannotCreateFolder {
+            folder: parent.to_path_buf(),
+            reason: err.to_string(),
+        })?;
+    }
     fs::write(path_on_disk, data).expect("cannot save file");
     super::make_file_executable(path_on_disk)?;
     Ok(Executable(path_on_disk.to_path_buf()))
