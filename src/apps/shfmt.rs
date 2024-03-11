678f08208faffcabc3bd6548cd4ece328a7aff29
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-11 11:29:15 -0500

    shfmt.rs

--- src/apps/shfmt.rs
+++ src/apps/shfmt.rs
@@ -1,12 +1,10 @@
 use super::{AnalyzeResult, App};
 use crate::config::{AppName, Version};
 use crate::hosting::github_releases;
-use crate::install::compile_go::{compile_go, CompileArgs};
-use crate::install::executable::{self, InstallArgs};
+use crate::install::Method;
 use crate::platform::{Cpu, Os, Platform};
 use crate::subshell::Executable;
-use crate::yard::Yard;
-use crate::{regexp, Output, Result};
+use crate::{install, regexp, Output, Result};
 use const_format::formatcp;
 
 pub struct Shfmt {}
@@ -23,31 +21,31 @@ impl App for Shfmt {
         formatcp!("https://github.com/{ORG}/{REPO}")
     }
 
-    fn install(&self, version: &Version, platform: Platform, yard: &Yard, output: &dyn Output) -> Result<Option<Executable>> {
-        let name = self.name();
-        let result = executable::install(InstallArgs {
-            app_name: &name,
-            artifact_url: download_url(version, platform),
-            filepath_on_disk: yard.create_app_folder(&name, version)?.join(self.executable_filepath(platform)),
-            output,
-        })?;
-        if result.is_some() {
-            return Ok(result);
-        }
-        compile_go(CompileArgs {
-            import_path: format!("mvdan.cc/sh/v3/cmd/shfmt@v{version}"),
-            target_folder: &yard.app_folder(&name, version),
-            executable_filepath: self.executable_filepath(platform),
-            output,
-        })
+    // fn install(&self, version: &Version, platform: Platform, yard: &Yard, output: &dyn Output) -> Result<Option<Executable>> {
+    //     let name = self.name();
+    //     let result = executable::install(InstallArgs {
+    //         app_name: &name,
+    //         artifact_url: download_url(version, platform),
+    //         filepath_on_disk: yard.create_app_folder(&name, version)?.join(self.executable_filepath(platform)),
+    //         output,
+    //     })?;
+    //     if result.is_some() {
+    //         return Ok(result);
+    //     }
+    //     compile_go(CompileArgs {
+    //         import_path: ,
+    //         target_folder: &yard.app_folder(&name, version),
+    //         executable_filepath: self.executable_filepath(platform),
+    //         output,
+    //     })
+    // }
+
+    fn install_methods(&self) -> Vec<install::Method> {
+        vec![Method::DownloadExecutable(self), Method::CompileGoSource(self)]
     }
 
     fn latest_installable_version(&self, output: &dyn Output) -> Result<Version> {
         github_releases::latest(ORG, REPO, output)
-    }
-
-    fn load(&self, version: &Version, platform: Platform, yard: &Yard) -> Option<Executable> {
-        yard.load_app(&self.name(), version, &self.executable_filepath(platform))
     }
 
     fn installable_versions(&self, amount: usize, output: &dyn Output) -> Result<Vec<Version>> {
@@ -65,26 +63,28 @@ impl App for Shfmt {
     }
 }
 
-fn cpu_text(cpu: Cpu) -> &'static str {
-    match cpu {
-        Cpu::Arm64 => "arm64",
-        Cpu::Intel64 => "amd64",
+impl install::DownloadExecutable for Shfmt {
+    fn artifact_url(&self, version: &Version, platform: Platform) -> String {
+        let os = match platform.os {
+            Os::Linux => "linux",
+            Os::MacOS => "darwin",
+            Os::Windows => "windows",
+        };
+        let cpu = match platform.cpu {
+            Cpu::Arm64 => "arm64",
+            Cpu::Intel64 => "amd64",
+        };
+        let ext = match platform.os {
+            Os::Linux | Os::MacOS => "",
+            Os::Windows => ".exe",
+        };
+        format!("https://github.com/{ORG}/{REPO}/releases/download/v{version}/shfmt_v{version}_{os}_{cpu}{ext}")
     }
 }
 
-fn download_url(version: &Version, platform: Platform) -> String {
-    format!(
-        "https://github.com/{ORG}/{REPO}/releases/download/v{version}/shfmt_v{version}_{os}_{cpu}{ext}",
-        os = os_text(platform.os),
-        cpu = cpu_text(platform.cpu),
-        ext = ext_text(platform.os)
-    )
-}
-
-fn ext_text(os: Os) -> &'static str {
-    match os {
-        Os::Linux | Os::MacOS => "",
-        Os::Windows => ".exe",
+impl install::CompileFromGoSource for Shfmt {
+    fn import_path(&self, version: &Version) -> String {
+        format!("mvdan.cc/sh/v3/cmd/shfmt@v{version}")
     }
 }
 
@@ -96,23 +96,17 @@ fn identify(output: &str) -> bool {
     output.contains("shfmt formats shell programs")
 }
 
-fn os_text(os: Os) -> &'static str {
-    match os {
-        Os::Linux => "linux",
-        Os::MacOS => "darwin",
-        Os::Windows => "windows",
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use crate::config::Version;
+    use crate::install::DownloadExecutable;
     use crate::platform::{Cpu, Os, Platform};
 
     #[test]
-    fn download_url() {
+    fn artifact_url() {
+        let shfmt = super::Shfmt {};
         let platform = Platform { os: Os::MacOS, cpu: Cpu::Arm64 };
-        let have = super::download_url(&Version::from("3.7.0"), platform);
+        let have = shfmt.artifact_url(&Version::from("3.7.0"), platform);
         let want = "https://github.com/mvdan/sh/releases/download/v3.7.0/shfmt_v3.7.0_darwin_arm64";
         assert_eq!(have, want);
     }
