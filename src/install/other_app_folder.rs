f8162bd446ce0c11ca4343ca00b05a52298e0502
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-12 09:22:37 -0500

    other_app_folder.rs

--- src/install/other_app_folder.rs
+++ src/install/other_app_folder.rs
@@ -7,6 +7,7 @@ use crate::{apps, Result};
 
 /// defines the information needed for apps whose executable is shipped as part of another app
 pub trait InstallAnotherApp: App {
+    /// the application that ships the executable of this app
     fn app_to_install(&self) -> Box<dyn App>;
 
     /// location of this app's executable within the archive of the other app
