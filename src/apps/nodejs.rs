5f256e5bbc629c5305838815fa4383f2edbdaf5f
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-12 06:42:14 -0500

    nodejs.rs

--- src/apps/nodejs.rs
+++ src/apps/nodejs.rs
@@ -1,10 +1,10 @@
 use super::{AnalyzeResult, App};
 use crate::config::{AppName, Version};
 use crate::hosting::github_releases;
-use crate::install::Method;
+use crate::install::{self, Method};
 use crate::platform::{Cpu, Os, Platform};
+use crate::regexp;
 use crate::subshell::Executable;
-use crate::{install, regexp};
 use crate::{Output, Result};
 use std::path;
 
