4c5c7bca8350a33a932a3c89468283d4030d22f5
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-13 10:12:50 -0500

    mod.rs test.rs

--- src/cmd/test.rs
+++ src/cmd/test.rs
@@ -1,6 +1,10 @@
-use crate::Result;
+use crate::{apps, logger, platform, Result};
 use std::process::ExitCode;
 
 pub fn test(verbose: bool) -> Result<ExitCode> {
+    let apps = apps::all();
+    let log = logger::new(verbose);
+    let platform = platform::detect(log)?;
+    for app in apps {}
     Ok(ExitCode::SUCCESS)
 }
