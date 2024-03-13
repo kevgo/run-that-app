b966045bed6ae1465d07578295af1a3596fd6c3a
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-12 19:58:18 -0500

    output.rs

--- src/output/output.rs
+++ src/output/output.rs
@@ -27,32 +27,33 @@ macro_rules! fprint {
 
 fn display_verbose(event: Event) {
     match event {
-        Event::CpuIdentified { architecture } => println!("CPU id: {architecture}"),
-        Event::OsIdentified { name } => println!("OS id: {name}"),
-        Event::DownloadBegin { app, url } => println!("download {} from {}", app, url.cyan().bold()),
-        Event::DownloadSuccess => println!("finished download"),
-        Event::DownloadFail { code } => println!("download failed: {}", code.to_string().red().bold()),
-        Event::NotOnline => todo!(),
-        Event::DownloadNotFound => todo!(),
-        Event::CompileGoStart { go_path, args } => println!("{go_path} {}", args.join(" ")),
-        Event::CompileGoSuccess => todo!(),
-        Event::CompileRustStart { cargo_path, args } => println!("{} {}", cargo_path.to_string_lossy(), args.join(" ")),
-        Event::CompileRustSuccess => todo!(),
-        Event::GitHubApiRequestBegin { url } => todo!(),
-        Event::GitHubApiRequestFail { err } => todo!(),
-        Event::GlobalInstallSearch { binary } => todo!(),
-        Event::GlobalInstallFound { path } => todo!(),
-        Event::GlobalInstallNotFound => todo!(),
-        Event::ExecutableInstallSave => todo!(),
-        Event::ExecutableInstallSaveSuccess => todo!(),
-        Event::ExecutableInstallSaveFail => todo!(),
-        Event::GitHubApiRequestSuccess => todo!(),
-        Event::UpdateBegin { app } => todo!(),
-        Event::UpdateNewVersion { app, old_version, new_versin } => todo!(),
-        Event::UpdateAlreadyNewest { app } => todo!(),
         Event::ArchiveExtractBegin { archive_type } => todo!(),
         Event::ArchiveExtractSuccess => todo!(),
         Event::ArchiveExtractFailed { err } => todo!(),
+
+        Event::CpuIdentified { architecture } => eprintln!("CPU id: {architecture}"),
+        Event::OsIdentified { name } => eprintln!("OS id: {name}"),
+
+        Event::DownloadBegin { app, url } => eprintln!("download {} from {}", app, url.cyan().bold()),
+        Event::DownloadSuccess => eprintln!("finished download"),
+        Event::DownloadFail { code } => eprintln!("download failed: {}", code.to_string().red().bold()),
+        Event::DownloadNotFound => eprintln!("not found"),
+
+        Event::CompileGoStart { go_path, args } => eprintln!("{go_path} {}", args.join(" ")),
+        Event::CompileGoSuccess => eprintln!("Go compilation successful"),
+        Event::CompileRustStart { cargo_path, args } => eprintln!("{} {}", cargo_path.to_string_lossy(), args.join(" ")),
+        Event::CompileRustSuccess => eprintln!("Rust compilation successful"),
+
+        Event::ExecutableInstallSave => fprint!("saving ... "),
+        Event::ExecutableInstallSaveSuccess => eprintln!("{}", "ok".green().bold()),
+        Event::ExecutableInstallSaveFail => eprintln!("{}", "failed".red().bold()),
+
+        Event::GitHubApiRequestBegin { url } => eprintln!("Talking to GitHub API: {url}"),
+        Event::GitHubApiRequestSuccess => eprintln!("GitHub API request success"),
+        Event::GitHubApiRequestFail { err } => eprintln!("GitHub API request failed: {err}"),
+
+        Event::GlobalInstallSearch { binary } => fprint!("Looking for {} in the PATH ... ", binary.cyan().bold()),
+        Event::GlobalInstallFound { path } => eprintln!("{}", path.to_string_lossy().green().bold()),
         Event::GlobalInstallMatchingVersion {
             version_restriction,
             actual_version,
@@ -61,7 +62,14 @@ fn display_verbose(event: Event) {
             version_restriction,
             actual_version,
         } => todo!(),
+        Event::GlobalInstallNotFound => eprintln!("{}", "not found".red().bold()),
         Event::GlobalInstallNotIdentified { executable } => todo!(),
+
+        Event::NotOnline => eprintln!("{}", "not online".red().bold()),
+
+        Event::UpdateBegin { app } => todo!(),
+        Event::UpdateNewVersion { app, old_version, new_versin } => todo!(),
+        Event::UpdateAlreadyNewest { app } => todo!(),
     }
 }
 
