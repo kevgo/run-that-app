5a34c54dcc18b85bdd3fd31a8c97f98d19ad2146
Kevin Goslar <kevin.goslar@gmail.com>
2024-03-12 20:36:16 -0500

    output.rs

--- src/output/output.rs
+++ src/output/output.rs
@@ -28,17 +28,17 @@ macro_rules! fprint {
 fn display_verbose(event: Event) {
     #[allow(clippy::match_same_arms)]
     match event {
-        Event::ArchiveExtractBegin { archive_type } => fprint!("extracting {} ... ", archive_type.cyan().bold()),
-        Event::ArchiveExtractSuccess => eprintln!("{}", "ok".green().bold()),
-        Event::ArchiveExtractFailed { err } => eprintln!("{}", err.red().bold()),
+        Event::ArchiveExtractBegin { archive_type } => fprint!("extracting {} ... ", archive_type.cyan()),
+        Event::ArchiveExtractSuccess => eprintln!("{}", "ok".green()),
+        Event::ArchiveExtractFailed { err } => eprintln!("{}", err.red()),
 
-        Event::CpuIdentified { architecture } => eprintln!("CPU id: {}", architecture.cyan().bold()),
-        Event::OsIdentified { name } => eprintln!("OS id: {}", name.cyan().bold()),
+        Event::CpuIdentified { architecture } => eprintln!("CPU id: {}", architecture.cyan()),
+        Event::OsIdentified { name } => eprintln!("OS id: {}", name.cyan()),
 
-        Event::DownloadBegin { app, url } => eprintln!("download {} from {}", app.as_str().cyan().bold(), url.cyan()),
-        Event::DownloadSuccess => eprintln!("{}", "ok".green().bold()),
-        Event::DownloadFail { code } => eprintln!("{}", code.to_string().red().bold()),
-        Event::DownloadNotFound => eprintln!("{}", "not found".red().bold()),
+        Event::DownloadBegin { app, url } => eprintln!("download {} from {}", app.as_str().cyan(), url.cyan()),
+        Event::DownloadSuccess => eprintln!("{}", "ok".green()),
+        Event::DownloadFail { code } => eprintln!("{}", code.to_string().red()),
+        Event::DownloadNotFound => eprintln!("{}", "not found".red()),
 
         Event::CompileGoStart { go_path, args } => eprintln!("{go_path} {}", args.join(" ")),
         Event::CompileGoSuccess => eprintln!("Go compilation successful"),
@@ -47,30 +47,23 @@ fn display_verbose(event: Event) {
         Event::CompileRustSuccess => eprintln!("Rust compilation successful"),
 
         Event::ExecutableInstallSave => fprint!("saving ... "),
-        Event::ExecutableInstallSaveSuccess => eprintln!("{}", "ok".green().bold()),
-        Event::ExecutableInstallSaveFail { err } => eprintln!("{}", err.red().bold()),
+        Event::ExecutableInstallSaveSuccess => eprintln!("{}", "ok".green()),
+        Event::ExecutableInstallSaveFail { err } => eprintln!("{}", err.red()),
 
         Event::GitHubApiRequestBegin { url } => eprintln!("Talking to GitHub API: {url} ... "),
-        Event::GitHubApiRequestSuccess => eprintln!("{}", "ok".green().bold()),
-        Event::GitHubApiRequestFail { err } => eprintln!("{}", err.red().bold()),
+        Event::GitHubApiRequestSuccess => eprintln!("{}", "ok".green()),
+        Event::GitHubApiRequestFail { err } => eprintln!("{}", err.red()),
 
-        Event::GlobalInstallSearch { binary } => fprint!("Looking for {} in the PATH ... ", binary.cyan().bold()),
-        Event::GlobalInstallFound { path } => eprintln!("{}", path.to_string_lossy().green().bold()),
+        Event::GlobalInstallSearch { binary } => fprint!("Looking for {} in the PATH ... ", binary.cyan()),
+        Event::GlobalInstallFound { path } => eprintln!("{}", path.to_string_lossy().green()),
         Event::GlobalInstallMatchingVersion {
             version_restriction,
             actual_version,
         } => {
             if let Some(version) = actual_version {
-                eprintln!(
-                    "found version {} matching {}",
-                    version.as_str().green().bold(),
-                    version_restriction.to_string().cyan().bold()
-                );
+                eprintln!("found version {} matching {}", version.as_str().green(), version_restriction.to_string().cyan());
             } else {
-                eprintln!(
-                    "found an installation with unknown version but it matches {}",
-                    version_restriction.to_string().cyan().bold()
-                );
+                eprintln!("found an installation with unknown version but it matches {}", version_restriction.to_string().cyan());
             }
         }
         Event::GlobalInstallMismatchingVersion {
@@ -78,26 +71,19 @@ fn display_verbose(event: Event) {
             actual_version,
         } => {
             if let Some(version) = actual_version {
-                eprintln!(
-                    "found version {} that does not match {}",
-                    version.as_str().red().bold(),
-                    version_restriction.to_string().cyan().bold()
-                );
+                eprintln!("found version {} that does not match {}", version.as_str().red(), version_restriction.to_string().cyan());
             } else {
-                eprintln!(
-                    "found an installation with unknown version and it doesn't match {}",
-                    version_restriction.to_string().cyan().bold()
-                );
+                eprintln!("found an installation with unknown version and it doesn't match {}", version_restriction.to_string().cyan());
             }
         }
-        Event::GlobalInstallNotFound => eprintln!("{}", "not found".red().bold()),
+        Event::GlobalInstallNotFound => eprintln!("{}", "not found".red()),
         Event::GlobalInstallNotIdentified => eprintln!("not found "),
 
-        Event::NotOnline => eprintln!("{}", "not online".red().bold()),
+        Event::NotOnline => eprintln!("{}", "not online".red()),
 
-        Event::UpdateBegin { app } => eprintln!("updating {} ...", app.as_str().cyan().bold()),
-        Event::UpdateNewVersion { old_version, new_version } => eprintln!("{} -> {}", old_version.as_str().green().bold(), new_version.as_str().green().bold()),
-        Event::UpdateAlreadyNewest { app: _ } => eprintln!("{}", "up to date".green().bold()),
+        Event::UpdateBegin { app } => eprintln!("updating {} ...", app.as_str().cyan()),
+        Event::UpdateNewVersion { old_version, new_version } => eprintln!("{} -> {}", old_version.as_str().green(), new_version.as_str().green()),
+        Event::UpdateAlreadyNewest { app: _ } => eprintln!("{}", "up to date".green()),
     }
 }
 
@@ -105,16 +91,16 @@ fn display_normal(event: Event) {
     #[allow(clippy::match_same_arms)]
     match event {
         Event::ArchiveExtractBegin { archive_type: _ } => fprint!("extracting ... "),
-        Event::ArchiveExtractSuccess => eprintln!("{}", "ok".green().bold()),
-        Event::ArchiveExtractFailed { err } => eprintln!("{}", err.red().bold()),
+        Event::ArchiveExtractSuccess => eprintln!("{}", "ok".green()),
+        Event::ArchiveExtractFailed { err } => eprintln!("{}", err.red()),
 
         Event::CpuIdentified { architecture: _ } => {}
         Event::OsIdentified { name: _ } => {}
 
-        Event::DownloadBegin { app, url: _ } => fprint!("downloading {} ... ", app.as_str().cyan().bold()),
+        Event::DownloadBegin { app, url: _ } => fprint!("downloading {} ... ", app.as_str().cyan()),
         Event::DownloadSuccess => {}
-        Event::DownloadFail { code } => eprintln!("{}", code.to_string().red().bold()),
-        Event::DownloadNotFound => eprintln!("{}", "not found".red().bold()),
+        Event::DownloadFail { code } => eprintln!("{}", code.to_string().red()),
+        Event::DownloadNotFound => eprintln!("{}", "not found".red()),
 
         Event::CompileGoStart { go_path: _, args } => eprintln!("go {}", args.join(" ")),
         Event::CompileGoSuccess => {}
@@ -123,7 +109,7 @@ fn display_normal(event: Event) {
         Event::CompileRustSuccess => {}
 
         Event::GitHubApiRequestBegin { url: _ } => {}
-        Event::GitHubApiRequestFail { err } => eprintln!("GitHub API request failed: {}", err.red().bold()),
+        Event::GitHubApiRequestFail { err } => eprintln!("GitHub API request failed: {}", err.red()),
         Event::GitHubApiRequestSuccess => {}
 
         Event::GlobalInstallSearch { binary: _ } => {}
@@ -140,13 +126,13 @@ fn display_normal(event: Event) {
         Event::GlobalInstallNotIdentified => {}
 
         Event::ExecutableInstallSave => fprint!("saving ... "),
-        Event::ExecutableInstallSaveSuccess => eprintln!("{}", "ok".green().bold()),
-        Event::ExecutableInstallSaveFail { err } => eprintln!("{}", err.red().bold()),
+        Event::ExecutableInstallSaveSuccess => eprintln!("{}", "ok".green()),
+        Event::ExecutableInstallSaveFail { err } => eprintln!("{}", err.red()),
 
-        Event::NotOnline => eprintln!("{}", "not online".red().bold()),
+        Event::NotOnline => eprintln!("{}", "not online".red()),
 
         Event::UpdateBegin { app: _ } => {}
-        Event::UpdateNewVersion { old_version, new_version } => eprintln!("{} -> {}", old_version.as_str().green().bold(), new_version.as_str().green().bold()),
-        Event::UpdateAlreadyNewest { app: _ } => eprintln!("{}", "up to date".green().bold()),
+        Event::UpdateNewVersion { old_version, new_version } => eprintln!("{} -> {}", old_version.as_str().green(), new_version.as_str().green()),
+        Event::UpdateAlreadyNewest { app: _ } => eprintln!("{}", "up to date".green()),
     }
 }
