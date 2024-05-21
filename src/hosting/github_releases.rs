57e93d4ef39e8ac799892fea9175b29437d14c82
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-20 19:51:08 -0500

    github_releases.rs

--- src/hosting/github_releases.rs
+++ src/hosting/github_releases.rs
@@ -59,7 +59,8 @@ pub fn versions(org: &str, repo: &str, amount: usize, log: Log) -> Result<Vec<Ve
     log(Event::NotOnline);
     return Err(UserError::NotOnline);
   };
-  parse_versions_response(response.as_str().unwrap())
+  let response_text = response.as_str().map_err(|err| UserError::InvalidGitHubAPIResponse { err: err.to_string() })?;
+  parse_versions_response(response_text)
 }
 
 fn parse_versions_response(text: &str) -> Result<Vec<Version>> {
