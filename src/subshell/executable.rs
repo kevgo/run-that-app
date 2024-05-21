f2764611187ec4b860b911498d7b2d7238a7615f
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-20 21:25:01 -0500

    executable.rs

--- src/subshell/executable.rs
+++ src/subshell/executable.rs
@@ -39,8 +39,8 @@ impl Executable {
         });
       }
     };
-    let stdout = String::from_utf8(output.stdout).expect("command printed non unicode stdout");
-    let stderr = String::from_utf8(output.stderr).expect("command printed non unicode stderr");
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    let stderr = String::from_utf8_lossy(&output.stderr);
     let output = format!("{stdout}{stderr}");
     Ok(output)
   }
