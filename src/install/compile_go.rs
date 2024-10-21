18cf5046775d8218177d2fa3689c0729374a81d7
Kevin Goslar <kevin.goslar@gmail.com>
2024-10-21 12:48:18 -0500

    compile_go.rs

--- src/install/compile_go.rs
+++ src/install/compile_go.rs
@@ -4,6 +4,7 @@ use crate::logger::{Event, Log};
 use crate::prelude::*;
 use crate::yard::Yard;
 use std::io::ErrorKind;
+use std::path::PathBuf;
 use std::process::Command;
 use which::which;
 
@@ -16,9 +17,14 @@ pub trait CompileGoSource: App {
 
 /// installs the given Go-based application by compiling it from source
 pub fn run(app: &dyn CompileGoSource, version: &Version, yard: &Yard, log: Log) -> Result<bool> {
-  let Ok(go_path) = which("go") else {
-    return Ok(false);
-  };
+  if let Ok(system_go_path) = which("go") {
+    compile_using_system_go(system_go_path, app, version, yard, log)
+  } else {
+    compile_using_rta_go()
+  }
+}
+
+fn compile_using_system_go(go_path: PathBuf, app: &dyn CompileGoSource, version: &Version, yard: &Yard, log: Log) -> Result<bool> {
   let target_folder = yard.create_app_folder(&app.name(), version)?;
   let import_path = app.import_path(version);
   let go_args = vec!["install", &import_path];
@@ -44,7 +50,5 @@ pub fn run(app: &dyn CompileGoSource, version: &Version, yard: &Yard, log: Log) 
   log(Event::CompileGoSuccess);
   Ok(true)
 }
-
-fn compile_using_system_go() -> Result<bool> {}
 
 fn compile_using_rta_go() -> Result<bool> {}
