d3654ce604aad54de80eb8d465e3a88cfcea3284
Kevin Goslar <kevin.goslar@gmail.com>
2024-10-21 18:16:50 -0500

    error.rs compile_go.rs

--- src/install/compile_go.rs
+++ src/install/compile_go.rs
@@ -1,11 +1,13 @@
 use super::Outcome;
+use crate::apps::go::Go;
 use crate::apps::{self, App};
-use crate::cmd;
-use crate::config::Version;
+use crate::config::{AppName, RequestedVersion, RequestedVersions, Version};
 use crate::logger::{Event, Log};
 use crate::platform::Platform;
 use crate::prelude::*;
+use crate::subshell::Executable;
 use crate::yard::Yard;
+use crate::{cmd, config};
 use big_s::S;
 use std::io::ErrorKind;
 use std::path::PathBuf;
@@ -55,20 +57,47 @@ fn compile_using_system_go(go_path: PathBuf, app: &dyn CompileGoSource, version:
   Ok(Outcome::Installed)
 }
 
-fn compile_using_rta_go(app: &dyn CompileGoSource, platform: Platform, app_version: &Version, yard: &Yard, log: Log) -> Result<Outcome> {
-  // install RTA Go if needed
+fn compile_using_rta_go(app: &dyn CompileGoSource, platform: Platform, app_version: &Version, config: config::File, yard: &Yard, log: Log) -> Result<Outcome> {
+  let target_folder = yard.create_app_folder(&app.name(), app_version)?;
+  // get the Go version to use
   let go = apps::go::Go {};
-  let go_version = &crate::config::RequestedVersion::Yard(3);
-  cmd::run::load_or_install(&go, go_version, platform, yard, log);
+  let requested_go_versions = match config.lookup(&go.name()) {
+    Some(versions) => versions,
+    None => {
+      let versions = go.installable_versions(5, log)?;
+      RequestedVersions::new(versions.into_iter().map(RequestedVersion::from).collect())
+    }
+  };
+  // get the executable, install Go if needed
+  let Some(go_executable) = load_or_install_go(&go, requested_go_versions, platform, yard, log)? else {
+    return Ok(Outcome::NotInstalled);
+  };
   let import_path = app.import_path(app_version);
-  // TODO: run with env variable GOBIN=(yard.create_app_folder(&app.name(), version)?)
-  let _ = cmd::run(cmd::run::Args {
-    app_name: go.name(),
-    version: None,
-    app_args: vec![S("install"), import_path],
-    error_on_output: false,
-    optional: false,
-    verbose: false,
-  })?;
-  Ok(Outcome::NotInstalled)
+  let mut cmd = Command::new(go_executable.0);
+  let go_args = vec!["install", &import_path];
+  cmd.args(go_args);
+  cmd.env("GOBIN", target_folder);
+  let status = match cmd.status() {
+    Ok(status) => status,
+    Err(err) => match err.kind() {
+      ErrorKind::PermissionDenied => return Err(UserError::GoNoPermission),
+      ErrorKind::Interrupted => return Err(UserError::CompilationInterupted),
+      _ => return Err(UserError::CompilationError { reason: err.to_string() }),
+    },
+  };
+  if !status.success() {
+    log(Event::CompileGoFailed);
+    return Err(UserError::GoCompilationFailed);
+  }
+  log(Event::CompileGoSuccess);
+  Ok(Outcome::Installed)
+}
+
+fn load_or_install_go(go: &Go, requested_go_versions: RequestedVersions, platform: Platform, yard: &Yard, log: Log) -> Result<Option<Executable>> {
+  for requested_go_version in requested_go_versions {
+    if let Some(executable) = cmd::run::load_or_install(go, &requested_go_version, platform, yard, log)? {
+      return Ok(Some(executable));
+    }
+  }
+  Ok(None)
 }
