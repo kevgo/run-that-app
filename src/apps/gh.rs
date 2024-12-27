1bb8993245fec80a530a5cc9b9b04f491d305ab2
Kevin Goslar <kevin.goslar@gmail.com>
2024-12-26 19:06:37 -0600

    gh.rs

--- src/apps/gh.rs
+++ src/apps/gh.rs
@@ -37,7 +37,7 @@ impl App for Gh {
 
   fn analyze_executable(&self, executable: &Executable, log: Log) -> Result<AnalyzeResult> {
     let output = executable.run_output("-h", log)?;
-    if !identify(&output) {
+    if !output.contains("Work seamlessly with GitHub from the command line") {
       return Ok(AnalyzeResult::NotIdentified { output });
     }
     match extract_version(&executable.run_output("--version", log)?) {
@@ -84,10 +84,6 @@ fn ext_text(os: Os) -> &'static str {
 
 fn extract_version(output: &str) -> Result<&str> {
   regexp::first_capture(output, r"gh version (\d+\.\d+\.\d+)")
-}
-
-fn identify(output: &str) -> bool {
-  output.contains("Work seamlessly with GitHub from the command line")
 }
 
 fn os_text(os: Os) -> &'static str {
