322749f59bc4b9cc850c9c595f3f2c875ccaebd3
Kevin Goslar <kevin.goslar@gmail.com>
2025-01-22 06:46:46 -0600

    shellcheck.rs

--- src/applications/shellcheck.rs
+++ src/applications/shellcheck.rs
@@ -97,7 +97,7 @@ mod tests {
       );
       let want = run::Method::ThisApp {
         install_methods: vec![Method::DownloadArchive {
-          url: S("https://github.com/koalaman/shellcheck/releases/download/v0.9.0/shellcheck-v0.9.0.linux.x86_64.tar.gz"),
+          url: S("https://github.com/koalaman/shellcheck/releases/download/v0.9.0/shellcheck-v0.9.0.linux.x86_64.tar.xz"),
           bin_folder: BinFolder::Subfolder { path: S("shellcheck-v0.9.0") },
         }],
       };
