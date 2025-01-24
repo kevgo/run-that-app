925ef83146bf0cc6c74d83516d56ba2db7d00fc1
Kevin Goslar <kevin.goslar@gmail.com>
2025-01-23 20:34:58 -0600

    mod.rs

--- src/installation/mod.rs
+++ src/installation/mod.rs
@@ -10,7 +10,7 @@ use crate::configuration::{self, Version};
 use crate::logging::Log;
 use crate::platform::Platform;
 use crate::prelude::*;
-use crate::run::ExecutableFileName;
+use crate::run::ExecutableNamePlatform;
 use crate::yard::Yard;
 use std::fmt::Debug;
 use std::path::{Path, PathBuf};
@@ -60,7 +60,13 @@ impl Method {
   }
 
   /// provides possible locations of the given executable within the given app folder in the given  yard
-  pub fn executable_paths(&self, app_definition: &dyn AppDefinition, executable_filename: &ExecutableFileName, version: &Version, yard: &Yard) -> Vec<PathBuf> {
+  pub fn executable_paths(
+    &self,
+    app_definition: &dyn AppDefinition,
+    executable_filename: &ExecutableNamePlatform,
+    version: &Version,
+    yard: &Yard,
+  ) -> Vec<PathBuf> {
     let app_folder = yard.app_folder(&app_definition.name(), version);
     match self {
       Method::DownloadArchive { url: _, bin_folder } => bin_folder.executable_paths(&app_folder, executable_filename),
@@ -98,7 +104,7 @@ pub enum BinFolder {
 }
 
 impl BinFolder {
-  pub fn executable_paths(&self, app_folder: &Path, executable_name: &ExecutableFileName) -> Vec<PathBuf> {
+  pub fn executable_paths(&self, app_folder: &Path, executable_name: &ExecutableNamePlatform) -> Vec<PathBuf> {
     match self {
       BinFolder::RootOrSubfolders { options } => {
         let mut result = vec![app_folder.join(executable_name)];
