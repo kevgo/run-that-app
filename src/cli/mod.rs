f87ca4e5f8b5ac0c428b03ccc5be24dfb7a9ef69
Kevin Goslar <kevin.goslar@gmail.com>
2025-01-30 14:21:05 -0600

    arguments.rs command.rs mod.rs

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,13 +1,10 @@
 //! CLI arguments
 
 mod app_version;
-mod arguments;
 mod command;
 mod exit;
 
 use app_version::AppVersion;
-pub(crate) use arguments::parse;
 #[cfg(test)]
-pub(crate) use arguments::Arguments;
-pub(crate) use command::Command;
+pub(crate) use command::{parse, Command};
 pub(crate) use exit::exit;
