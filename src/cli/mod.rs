3771bace84d05ff551c4a5a8965aeaddf9f59ed4
Kevin Goslar <kevin.goslar@gmail.com>
2025-02-01 07:03:41 -0600

    mod.rs parse.rs

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,11 +1,11 @@
 //! CLI arguments
 
 mod app_version;
-mod arguments;
 mod command;
 mod exit;
+mod parse;
 
 use app_version::AppVersion;
-pub(crate) use arguments::parse;
 pub(crate) use command::Command;
 pub(crate) use exit::exit;
+pub(crate) use parse::parse;
