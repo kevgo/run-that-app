9e19023fe17869e3ba9f9ae72f93cf200fe4f663
Kevin Goslar <kevin.goslar@gmail.com>
2025-01-31 19:42:17 -0600

    rest

--- src/installation/download_executable.rs
+++ src/installation/download_executable.rs
@@ -1,28 +1,17 @@
 use super::Outcome;
 use crate::applications::AppDefinition;
-use crate::configuration::Version;
 use crate::logging::Log;
 use crate::platform::Platform;
 use crate::prelude::*;
-use crate::yard::Yard;
 use crate::{download, filesystem};
+use std::path::Path;
 
 /// downloads an uncompressed precompiled binary
-pub(crate) fn run(
-  app_definition: &dyn AppDefinition,
-  url: &str,
-  version: &Version,
-  platform: Platform,
-  optional: bool,
-  yard: &Yard,
-  log: Log,
-) -> Result<Outcome> {
+pub(crate) fn run(app_definition: &dyn AppDefinition, app_folder: &Path, url: &str, platform: Platform, optional: bool, log: Log) -> Result<Outcome> {
   let Some(artifact) = download::artifact(url, &app_definition.name(), optional, log)? else {
     return Ok(Outcome::NotInstalled);
   };
-  let filepath_on_disk = yard
-    .create_app_folder(&app_definition.name(), version)?
-    .join(app_definition.default_executable_filename().platform_path(platform.os));
+  let filepath_on_disk = app_folder.join(app_definition.default_executable_filename().platform_path(platform.os));
   filesystem::save_executable(artifact.data, &filepath_on_disk, log)?;
   Ok(Outcome::Installed)
 }
