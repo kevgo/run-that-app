de3637b5d020ddba5e51e1022252a44ddec789c7
Kevin Goslar <kevin.goslar@gmail.com>
2025-02-03 18:16:21 -0600

    Cargo.lock Cargo.toml README RELEASE download.ps1 download.sh

--- download.ps1
+++ download.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-Set-Variable -Name "version" -Value "0.10.6" -Option Constant
+Set-Variable -Name "version" -Value "0.11.0" -Option Constant
 
 function Welcome() {
   Write-Output "RUN-THAT-APP DOWNLOAD SCRIPT"
--- download.sh
+++ download.sh
@@ -9,7 +9,7 @@ print_welcome() {
 	echo
 }
 
-VERSION="0.10.6" # the version of run-that-app to download
+VERSION="0.11.0" # the version of run-that-app to download
 TMP_DIR=./run_that_app_install
 
 main() {
