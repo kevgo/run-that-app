dec7a14e421ce7eb9da7add5ef022c51d64d8f0d
Kevin Goslar <kevin.goslar@gmail.com>
2025-02-17 13:30:30 -0600

    run.rs check_output.rs executable_call.rs mod.rs

--- src/run/mod.rs
+++ src/run/mod.rs
@@ -1,6 +1,5 @@
 //! all the logic around running applications
 
-mod check_output;
 mod executable_args;
 mod executable_call;
 mod executable_name_platform;
@@ -8,8 +7,6 @@ mod executable_name_unix;
 mod executable_path;
 mod exit_status_to_code;
 mod method;
-
-pub(crate) use check_output::check_output;
 pub(crate) use executable_args::ExecutableArgs;
 pub(crate) use executable_call::{ExecutableCall, ExecutableCallDefinition};
 pub(crate) use executable_name_platform::ExecutableNamePlatform;
