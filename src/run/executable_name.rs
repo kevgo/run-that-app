a7d927c63e406f765a9b5adb427cb5aadfbfef01
Kevin Goslar <kevin.goslar@gmail.com>
2025-02-17 18:56:38 -0600

    Consolidate types into fewer files

--- src/run/executable_name.rs
+++ src/run/executable_name.rs
@@ -1,6 +1,6 @@
-use super::ExecutableNamePlatform;
 use crate::platform::Os;
 use std::fmt::Display;
+use std::path::Path;
 
 /// the unix name of an executable
 #[derive(Clone, Debug, PartialEq)]
@@ -37,5 +37,27 @@ impl From<&str> for ExecutableNameUnix {
 impl From<String> for ExecutableNameUnix {
   fn from(value: String) -> Self {
     ExecutableNameUnix(value)
+  }
+}
+
+/// The platform-specific filename of an executable.
+/// On Windows: "unix-executable-name.exe"
+pub(crate) struct ExecutableNamePlatform(String);
+
+impl From<String> for ExecutableNamePlatform {
+  fn from(value: String) -> Self {
+    ExecutableNamePlatform(value.to_string())
+  }
+}
+
+impl AsRef<Path> for ExecutableNamePlatform {
+  fn as_ref(&self) -> &Path {
+    Path::new(&self.0)
+  }
+}
+
+impl Display for ExecutableNamePlatform {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    f.write_str(&self.0)
   }
 }
--- src/run/mod.rs
+++ src/run/mod.rs
@@ -1,15 +1,11 @@
 //! all the logic around running applications
 
 mod executable;
-mod executable_args;
 mod executable_call;
-mod executable_name_platform;
-mod executable_name_unix;
+mod executable_name;
 mod method;
 
 pub(crate) use executable::Executable;
-pub(crate) use executable_args::ExecutableArgs;
-pub(crate) use executable_call::{ExecutableCall, ExecutableCallDefinition};
-pub(crate) use executable_name_platform::ExecutableNamePlatform;
-pub(crate) use executable_name_unix::ExecutableNameUnix;
+pub(crate) use executable_call::{ExecutableArgs, ExecutableCall, ExecutableCallDefinition};
+pub(crate) use executable_name::{ExecutableNamePlatform, ExecutableNameUnix};
 pub(crate) use method::Method;
