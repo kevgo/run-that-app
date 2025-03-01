d442f329dc90a705488f043b41d7dbc75f073e65
Kevin Goslar <kevin.goslar@gmail.com>
2025-02-28 19:56:50 -0600

    command.rs

--- src/cli/command.rs
+++ src/cli/command.rs
@@ -1,16 +1,16 @@
-use crate::commands::{self, available, run, test, update, versions};
+use crate::commands::{add, available, run, test, update, versions, which};
 
 /// the main commands that run-this-app can execute
 #[derive(Debug, PartialEq)]
 pub(crate) enum Command {
-  Add(commands::add::Args),
+  Add(add::Args),
   AppsLong,
   AppsShort,
   Available(available::Args),
   RunApp(run::Args),
   DisplayHelp,
   Test(test::Args),
-  Which(commands::which::Args),
+  Which(which::Args),
   Update(update::Args),
   Version,
   Versions(versions::Args),
