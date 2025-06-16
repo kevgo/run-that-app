79bcbfd79668422ddaba6f2b384e8fbf60a91b30
Kevin Goslar <kevin.goslar@gmail.com>
2025-06-16 14:39:03 -0500

    v0.18 (#486)

--- download.ps1
+++ download.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-Set-Variable -Name "version" -Value "0.17.0" -Option Constant
+Set-Variable -Name "version" -Value "0.18.0" -Option Constant
 
 function Welcome() {
   Write-Output "RUN-THAT-APP DOWNLOAD SCRIPT"
