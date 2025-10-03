63d3d9c63bf17c1cba2c2a9d8c94cc9c48c8b962
Kevin Goslar <kevin.goslar@gmail.com>
2025-10-03 15:15:45 -0500

    mod.rs add.rs

--- src/commands/add.rs
+++ src/commands/add.rs
@@ -12,7 +12,7 @@ pub(crate) fn add(args: Args, apps: &Apps) -> Result<ExitCode> {
   } else {
     configuration::File::create(&app.app_name(), &version.clone())?;
   }
-  println!("added {}@{} to {}", &app, &version, configuration::FILE_NAME);
+  println!("added {}@{} to {}", app.name(), &version, configuration::FILE_NAME);
   Ok(ExitCode::SUCCESS)
 }
 
