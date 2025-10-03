69a99549dc6653fee41082ae9de59fc2908094e1
Kevin Goslar <kevin.goslar@gmail.com>
2025-10-03 15:14:37 -0500

    test.rs

--- src/commands/test.rs
+++ src/commands/test.rs
@@ -9,12 +9,12 @@ use std::io;
 use std::process::ExitCode;
 
 pub(crate) fn test(args: &mut Args, apps: &Apps) -> Result<ExitCode> {
-  find_duplicate_app_names(&apps)?;
+  find_duplicate_app_names(apps)?;
   let log = logging::new(args.verbose);
   let platform = platform::detect(log)?;
   let temp_folder = tempfile::tempdir().map_err(|err| UserError::CannotCreateTempDir { err: err.to_string() })?;
   let yard = Yard::load_or_create(temp_folder.path())?;
-  let config_file = configuration::File::load(&apps)?;
+  let config_file = configuration::File::load(apps)?;
   for app in apps {
     if let Some(start_app_name) = &args.start_at_app {
       if &app.app_name() != start_app_name {
