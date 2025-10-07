10c27def7c578eebe3af196891bc1ae3d06df273
Kevin Goslar <kevin.goslar@gmail.com>
2025-10-07 06:05:30 -0500

    Sort config file

--- src/configuration/app_versions.rs
+++ src/configuration/app_versions.rs
@@ -6,3 +6,9 @@ pub(crate) struct AppVersions {
   pub(crate) app_name: ApplicationName,
   pub(crate) versions: RequestedVersions,
 }
+
+impl Ord for AppVersions {
+  fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+    // TODO: sort first by app name, then by version
+  }
+}
