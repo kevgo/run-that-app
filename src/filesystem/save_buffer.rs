cad3697784bb1b6e62536b6ff2d632b1f6cacd97
Kevin Goslar <kevin.goslar@gmail.com>
2025-12-31 09:09:50 -0600

    save_buffer.rs

--- src/filesystem/save_buffer.rs
+++ src/filesystem/save_buffer.rs
@@ -1,4 +1,3 @@
-use crate::error::Result;
 use crate::executables::Executable;
 use crate::filesystem;
 use crate::logging::{Event, Log};
@@ -6,12 +5,12 @@ use std::fs;
 use std::path::Path;
 
 /// saves the given file data as an executable file
-pub(crate) fn save_executable(data: Vec<u8>, path_on_disk: &Path, log: Log) -> Result<Executable> {
+pub(crate) fn save_executable(data: Vec<u8>, path_on_disk: &Path, log: Log) -> Executable {
   log(Event::ExecutableInstallSaveBegin);
   match fs::write(path_on_disk, data) {
     Ok(()) => log(Event::ExecutableInstallSaveSuccess),
     Err(err) => log(Event::ExecutableInstallSaveFail { err: err.to_string() }),
   }
   filesystem::make_executable(path_on_disk);
-  Ok(Executable::from(path_on_disk))
+  Executable::from(path_on_disk)
 }
