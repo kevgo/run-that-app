a46138f5a6bc772b9b71f8e32102cdd9f35d3a82
Kevin Goslar <kevin.goslar@gmail.com>
2025-12-31 07:44:30 -0600

    gz.rs

--- src/archives/gz.rs
+++ src/archives/gz.rs
@@ -3,7 +3,7 @@ use crate::error::{Result, UserError};
 use crate::logging::{Event, Log};
 use flate2::read::GzDecoder;
 use std::fs::File;
-use std::io::{self, Read, Write};
+use std::io;
 use std::path::{Path, PathBuf};
 
 /// a .gz file downloaded from the internet, containing a single executable
@@ -15,15 +15,6 @@ pub(crate) struct Gz {
 impl Archive for Gz {
   fn extract_all(&self, target_dir: &Path, log: Log) -> Result<()> {
     log(Event::ArchiveExtractBegin { archive_type: "gz" });
-
-    // decompress the gzip data
-    let mut gz_decoder = GzDecoder::new(io::Cursor::new(&self.data));
-    let mut decompressed_data = Vec::new();
-
-    if let Err(err) = gz_decoder.read_to_end(&mut decompressed_data) {
-      log(Event::ArchiveExtractFailed { err: err.to_string() });
-      return Err(UserError::ArchiveCannotExtract { reason: err.to_string() });
-    }
 
     // determine output filename by removing .gz extension
     let filename_path = PathBuf::from(&self.filename);
@@ -37,10 +28,11 @@ impl Archive for Gz {
 
     let output_path = target_dir.join(output_filename);
 
-    // write decompressed data to file
+    // stream decompressed data directly to file
+    let mut gz_decoder = GzDecoder::new(io::Cursor::new(&self.data));
     match File::create(&output_path) {
       Ok(mut file) => {
-        if let Err(err) = file.write_all(&decompressed_data) {
+        if let Err(err) = io::copy(&mut gz_decoder, &mut file) {
           log(Event::ArchiveExtractFailed { err: err.to_string() });
           return Err(UserError::ArchiveCannotExtract { reason: err.to_string() });
         }
