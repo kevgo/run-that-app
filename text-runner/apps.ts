3b88e53ed56eb09b52a91f08ba228a163e2728c1
Kevin Goslar <kevin.goslar@gmail.com>
2026-01-06 07:20:51 -0600

    apps.ts apps.test.ts

--- text-runner/apps.ts
+++ text-runner/apps.ts
@@ -3,43 +3,43 @@ import { execSync } from "node:child_process";
 import * as textRunner from "text-runner";
 
 export function apps(action: textRunner.actions.Args) {
-	action.name("verify installable applications")
-	const installableApps = loadInstallableApps();
-	const documentedApps = loadDocumentedApps(action.region);
-	deepEqual(installableApps, documentedApps);
+  action.name("verify installable applications");
+  const installableApps = loadInstallableApps();
+  const documentedApps = loadDocumentedApps(action.region);
+  deepEqual(installableApps, documentedApps);
 }
 
 export class App {
-	name: string;
-	url: string;
+  name: string;
+  url: string;
 }
 
 function loadDocumentedApps(region: textRunner.ast.NodeList): App[] {
-	const result = []
-	for (const listItemOpener of region.nodesOfTypes("list_item_open")) {
-		const listItemNodes = region.nodesFor(listItemOpener)
-		const linkOpener = listItemNodes.nodeOfTypes("link_open")
-		result.push({
-			name: listItemNodes.nodesFor(linkOpener).text(),
-			url: linkOpener.attributes["href"],
-		})
-	}
-	return result
+  const result = [];
+  for (const listItemOpener of region.nodesOfTypes("list_item_open")) {
+    const listItemNodes = region.nodesFor(listItemOpener);
+    const linkOpener = listItemNodes.nodeOfTypes("link_open");
+    result.push({
+      name: listItemNodes.nodesFor(linkOpener).text(),
+      url: linkOpener.attributes["href"],
+    });
+  }
+  return result;
 }
 
 function loadInstallableApps(): App[] {
-	return parseApps(queryApps());
+  return parseApps(queryApps());
 }
 
 export function parseApps(output: string): App[] {
-	return output.trim().split("\n").map(parseLine);
+  return output.trim().split("\n").map(parseLine);
 }
 
 export function parseLine(line: string): App {
-	const [name, url] = line.split(" ");
-	return { name, url };
+  const [name, url] = line.split(" ");
+  return { name, url };
 }
 
 function queryApps(): string {
-	return execSync("cargo run -- --apps", { encoding: "utf-8" })
+  return execSync("cargo run -- --apps", { encoding: "utf-8" });
 }
--- text-runner/lib/apps.test.ts
+++ text-runner/lib/apps.test.ts
@@ -1,35 +1,36 @@
 import { deepStrictEqual } from "node:assert/strict";
-import { parseApps, parseLine, App } from "../apps.ts";
-import { suite, test } from "node:test"
+import { suite, test } from "node:test";
+import { App, parseApps, parseLine } from "../apps.ts";
 
 suite("parseApps", () => {
-	test("many apps", () => {
-		const give = `
+  test("many apps", () => {
+    const give = `
 actionlint https://github.com/actionlint/actionlint
 alphavet https://github.com/alphavet/alphavet
-`
-		const have = parseApps(give);
-		const want: App[] = [
-			{
-				name: "actionlint",
-				url: "https://github.com/actionlint/actionlint",
-			}, {
-				name: "alphavet",
-				url: "https://github.com/alphavet/alphavet",
-			},
-		]
-		deepStrictEqual(have, want);
-	})
-})
+`;
+    const have = parseApps(give);
+    const want: App[] = [
+      {
+        name: "actionlint",
+        url: "https://github.com/actionlint/actionlint",
+      },
+      {
+        name: "alphavet",
+        url: "https://github.com/alphavet/alphavet",
+      },
+    ];
+    deepStrictEqual(have, want);
+  });
+});
 
 suite("parseLine", () => {
-	test("name and url", () => {
-		const give = "actionlint https://github.com/actionlint/actionlint"
-		const have = parseLine(give);
-		const want: App = {
-			name: "actionlint",
-			url: "https://github.com/actionlint/actionlint",
-		}
-		deepStrictEqual(have, want);
-	})
-})
+  test("name and url", () => {
+    const give = "actionlint https://github.com/actionlint/actionlint";
+    const have = parseLine(give);
+    const want: App = {
+      name: "actionlint",
+      url: "https://github.com/actionlint/actionlint",
+    };
+    deepStrictEqual(have, want);
+  });
+});
