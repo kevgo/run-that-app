d36142171a3123968e6f59224be2bb69bf472dab
Kevin Goslar <kevin.goslar@gmail.com>
2026-01-07 20:57:14 -0600

    reinstall.rs

--- src/commands/reinstall.rs
+++ src/commands/reinstall.rs
@@ -6,6 +6,6 @@ use std::process::ExitCode;
 
 pub(crate) fn reinstall(args: install::Args, apps: &Apps) -> Result<ExitCode> {
   let yard = Yard::load_or_create(&yard::production_location()?)?;
-  let _ = yard.delete_app_folder(&args.app_name);
+  yard.delete_app_folder(&args.app_name)?;
   install(args, apps)
 }
