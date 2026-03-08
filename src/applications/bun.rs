d35be71f5e0744a50e2b2772a148d7cd0bf72c1f
Kevin Goslar <kevin.goslar@gmail.com>
2026-03-08 10:23:22 -0500

    bun.rs

--- src/applications/bun.rs
+++ src/applications/bun.rs
@@ -44,12 +44,11 @@ impl AppDefinition for Bun {
   }
 
   fn installable_versions(&self, amount: usize, log: Log) -> Result<Vec<Version>> {
-    let raw_versions = github_releases::versions(ORG, REPO, amount, log)?;
-    Ok(raw_versions.into_iter().map(|version| version.strip_prefix("bun-v")).collect())
+    github_releases::versions(ORG, REPO, amount, "bun-v", log)
   }
 
   fn latest_installable_version(&self, log: Log) -> Result<Version> {
-    github_releases::latest(ORG, REPO, log)
+    github_releases::latest(ORG, REPO, "bun-v", log)
   }
 
   fn analyze_executable(&self, executable: &Executable, log: Log) -> Result<AnalyzeResult> {
