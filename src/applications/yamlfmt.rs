45e7b1b624e6aa6ab6abcb987bb9aaa3060208da
Kevin Goslar <kevin.goslar@gmail.com>
2026-05-09 05:55:10 -0500

    mod.rs uv.rs yamlfmt.rs

---
+++ src/applications/yamlfmt.rs
@@ -0,0 +1,118 @@
+use const_format::formatcp;
+
+use super::{AnalyzeResult, AppDefinition, ApplicationName};
+use crate::configuration::Version;
+use crate::error::Result;
+use crate::executables::{Executable, RunMethod};
+use crate::hosting::github_releases;
+use crate::installation::{BinFolder, Method};
+use crate::platform::{Cpu, Os, Platform};
+use crate::{Log, strings};
+
+#[derive(Clone)]
+pub(crate) struct Yamlfmt {}
+
+const ORG: &str = "google";
+const REPO: &str = "yamlfmt";
+const TAG_PREFIX: &str = "v";
+
+impl AppDefinition for Yamlfmt {
+  fn name(&self) -> ApplicationName {
+    "yamlfmt".into()
+  }
+
+  fn homepage(&self) -> &'static str {
+    formatcp!("https://github.com/{ORG}/{REPO}")
+  }
+
+  fn run_method(&self, version: &Version, platform: Platform) -> RunMethod {
+    let os = match platform.os {
+      Os::Linux => "Linux",
+      Os::MacOS => "Darwin",
+      Os::Windows => "Windows",
+    };
+    let cpu = match platform.cpu {
+      Cpu::Arm64 => "arm64",
+      Cpu::Intel64 => "x86_64",
+    };
+    RunMethod::ThisApp {
+      install_methods: vec![Method::DownloadArchive {
+        url: format!("https://github.com/{ORG}/{REPO}/releases/download/{TAG_PREFIX}{version}/yamlfmt_{version}_{os}_{cpu}.tar.gz").into(),
+        bin_folder: BinFolder::Root,
+      }],
+    }
+  }
+
+  fn latest_installable_version(&self, log: Log) -> Result<Version> {
+    github_releases::latest(ORG, REPO, TAG_PREFIX, log)
+  }
+
+  fn installable_versions(&self, amount: usize, log: Log) -> Result<Vec<Version>> {
+    github_releases::versions(ORG, REPO, amount, TAG_PREFIX, log)
+  }
+
+  fn analyze_executable(&self, executable: &Executable, log: Log) -> Result<AnalyzeResult> {
+    let output = executable.run_output(&["-h"], log)?;
+    if !output.contains("Rclone syncs files to and from cloud storage providers") {
+      return Ok(AnalyzeResult::NotIdentified { output });
+    }
+    let output = executable.run_output(&["version"], log)?;
+    match extract_version(&output) {
+      Ok(version) => Ok(AnalyzeResult::IdentifiedWithVersion(version.into())),
+      Err(_) => Ok(AnalyzeResult::IdentifiedButUnknownVersion),
+    }
+  }
+}
+
+fn extract_version(output: &str) -> Result<&str> {
+  strings::first_capture(output, r"rclone v(\d+\.\d+\.\d+)")
+}
+
+#[cfg(test)]
+mod tests {
+
+  mod install_methods {
+    use crate::applications::AppDefinition;
+    use crate::applications::yamlfmt::Yamlfmt;
+    use crate::configuration::Version;
+    use crate::executables::RunMethod;
+    use crate::installation::{BinFolder, Method};
+    use crate::platform::{Cpu, Os, Platform};
+
+    #[test]
+    fn linux_arm() {
+      let have = (Yamlfmt {}).run_method(
+        &Version::from("0.21.0"),
+        Platform {
+          os: Os::Linux,
+          cpu: Cpu::Arm64,
+        },
+      );
+      let want = RunMethod::ThisApp {
+        install_methods: vec![Method::DownloadArchive {
+          url: "https://github.com/google/yamlfmt/releases/download/v0.21.0/yamlfmt_0.21.0_Linux_arm64.tar.gz".into(),
+          bin_folder: BinFolder::Root,
+        }],
+      };
+      assert_eq!(have, want);
+    }
+
+    #[test]
+    fn windows_intel() {
+      let have = (Yamlfmt {}).run_method(
+        &Version::from("0.21.0"),
+        Platform {
+          os: Os::Windows,
+          cpu: Cpu::Intel64,
+        },
+      );
+      let want = RunMethod::ThisApp {
+        install_methods: vec![Method::DownloadArchive {
+          url: "https://github.com/google/yamlfmt/releases/download/v0.21.0/yamlfmt_0.21.0_Windows_x86_64.tar.gz".into(),
+          bin_folder: BinFolder::Root,
+        }],
+      };
+      assert_eq!(have, want);
+    }
+  }
+}
