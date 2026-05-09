90fa88460cbfa1544f866124af252f79d68d7c66
Kevin Goslar <kevin.goslar@gmail.com>
2026-05-09 08:09:35 -0500

    Inline unnecessary function

--- src/filesystem/set_executable_bit.rs
+++ src/filesystem/set_executable_bit.rs
@@ -1,23 +1,9 @@
 use std::path::Path;
 
-// TODO: merge these three functions
-
 /// Makes the given file executable by setting appropriate permissions.
 /// Ignores non-existing files.
+#[cfg(not(windows))]
 pub(crate) fn set_executable_bit(filepath: &Path) {
-  #[cfg(not(windows))]
-  return set_executable_bit_unix(filepath);
-  #[cfg(windows)]
-  return set_executable_bit_windows(filepath);
-}
-
-#[cfg(windows)]
-fn set_executable_bit_windows(_filepath: &Path) {
-  // Windows does not have file permissions --> nothing to do here
-}
-
-#[cfg(unix)]
-fn set_executable_bit_unix(filepath: &Path) {
   use std::fs;
   use std::os::unix::fs::PermissionsExt;
   let Ok(executable_file) = fs::File::open(filepath) else { return };
@@ -29,4 +15,10 @@ fn set_executable_bit_unix(filepath: &Path) {
   }
   permissions.set_mode(0o744);
   let _ = fs::set_permissions(filepath, permissions);
+}
+
+/// Does nothing on Windows.
+#[cfg(windows)]
+fn set_executable_bit(_filepath: &Path) {
+  // Windows does not have file permissions --> nothing to do here
 }
