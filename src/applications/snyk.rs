85698327eef3aaa6da8d9ceefb540f13949fe061
Kevin Goslar <kevin.goslar@gmail.com>
2026-05-10 07:00:51 -0500

    rumdl.rs scc.rs shellcheck.rs shfmt.rs snyk.rs staticcheck.rs taplo.rs tikibase.rs ty.rs uv.rs yamlfmt.rs

--- src/applications/snyk.rs
+++ src/applications/snyk.rs
@@ -1,5 +1,5 @@
 use super::{AnalyzeResult, AppDefinition, ApplicationName};
-use crate::configuration::Version;
+use crate::configuration::{TagFormat, Version};
 use crate::error::Result;
 use crate::executables::{Executable, RunMethod};
 use crate::hosting::github_releases;
@@ -13,7 +13,6 @@ pub(crate) struct Snyk {}
 
 const ORG: &str = "snyk";
 const REPO: &str = "cli";
-const TAG_PREFIX: &str = "v";
 
 impl AppDefinition for Snyk {
   fn name(&self) -> ApplicationName {
@@ -38,19 +37,20 @@ impl AppDefinition for Snyk {
       Os::Linux | Os::MacOS => "",
       Os::Windows => ".exe",
     };
+    let tag = self.tag_format().format_version(version);
     RunMethod::ThisApp {
       install_methods: vec![Method::DownloadExecutable {
-        url: format!("https://github.com/{ORG}/{REPO}/releases/download/{TAG_PREFIX}{version}/snyk-{os}{cpu}{ext}").into(),
+        url: format!("https://github.com/{ORG}/{REPO}/releases/download/{tag}/snyk-{os}{cpu}{ext}").into(),
       }],
     }
   }
 
   fn latest_installable_version(&self, log: Log) -> Result<Version> {
-    github_releases::latest(ORG, REPO, TAG_PREFIX, log)
+    github_releases::latest(ORG, REPO, &self.tag_format(), log)
   }
 
   fn installable_versions(&self, amount: usize, log: Log) -> Result<Vec<Version>> {
-    github_releases::versions(ORG, REPO, amount, TAG_PREFIX, log)
+    github_releases::versions(ORG, REPO, amount, &self.tag_format(), log)
   }
 
   fn analyze_executable(&self, executable: &Executable, log: Log) -> Result<AnalyzeResult> {
@@ -62,6 +62,10 @@ impl AppDefinition for Snyk {
       Ok(version) => Ok(AnalyzeResult::IdentifiedWithVersion(version.into())),
       Err(_) => Ok(AnalyzeResult::IdentifiedButUnknownVersion),
     }
+  }
+
+  fn tag_format(&self) -> TagFormat {
+    TagFormat::PrefixV
   }
 }
 
