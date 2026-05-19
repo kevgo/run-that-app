b3d3cea4663d45334ccc22d5401e1952729ac416
Kevin Goslar <kevin.goslar@gmail.com>
2026-05-18 19:11:15 -0500

    lefthook.rs mod.rs

---
+++ src/applications/lefthook.rs
@@ -0,0 +1,217 @@
+use super::{AnalyzeResult, AppDefinition, ApplicationName};
+use crate::configuration::{TagFormat, Version};
+use crate::error::Result;
+use crate::executables::{Executable, RunMethod};
+use crate::hosting::github_releases;
+use crate::installation::{BinFolder, Method};
+use crate::platform::{Cpu, Os, Platform};
+use crate::{Log, executables, strings};
+use const_format::formatcp;
+
+#[derive(Clone)]
+pub(crate) struct Lefthook {}
+
+const ORG: &str = "evilmartians";
+const REPO: &str = "lefthook";
+
+impl AppDefinition for Lefthook {
+  fn name(&self) -> ApplicationName {
+    "lefthook".into()
+  }
+
+  fn homepage(&self) -> &'static str {
+    formatcp!("https://github.com/{ORG}/{REPO}")
+  }
+
+  fn executable_filename(&self) -> executables::ExecutableNameUnix {
+    executables::ExecutableNameUnix::from("lefthook")
+  }
+
+  fn run_method(&self, version: &Version, platform: Platform) -> RunMethod {
+    let cpu = match platform.cpu {
+      Cpu::Arm64 => "arm64",
+      Cpu::Intel64 => "x86_64",
+    };
+    let os = match platform.os {
+      Os::Linux => "Linux",
+      Os::MacOS => "MacOS",
+      Os::Windows => "Windows",
+    };
+    let tag = self.tag_format().format_version(version);
+    RunMethod::ThisApp {
+      install_methods: vec![Method::DownloadArchive {
+        url: format!("https://github.com/{ORG}/{REPO}/releases/download/{tag}/lefthook_{version}_{os}_{cpu}.gz").into(),
+        bin_folder: BinFolder::Subfolder {
+          path: format!("ripgrep-{version}-{cpu}-{os}").into(),
+        },
+      }],
+    }
+  }
+
+  fn installable_versions(&self, amount: usize, log: Log) -> Result<Vec<Version>> {
+    github_releases::versions(ORG, REPO, amount, &self.tag_format(), log)
+  }
+
+  fn latest_installable_version(&self, log: Log) -> Result<Version> {
+    github_releases::latest(ORG, REPO, &self.tag_format(), log)
+  }
+
+  fn analyze_executable(&self, executable: &Executable, log: Log) -> Result<AnalyzeResult> {
+    let output = executable.run_output(&["-h"], log)?;
+    if !output.contains("lefthook") {
+      return Ok(AnalyzeResult::NotIdentified { output });
+    }
+    match extract_version(&output) {
+      Ok(version) => Ok(AnalyzeResult::IdentifiedWithVersion(version.into())),
+      Err(_) => Ok(AnalyzeResult::IdentifiedButUnknownVersion),
+    }
+  }
+
+  fn tag_format(&self) -> TagFormat {
+    TagFormat::Plain
+  }
+}
+
+fn extract_version(output: &str) -> Result<&str> {
+  strings::first_capture(output, r"lefthook (\d+\.\d+\.\d+)")
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::UserError;
+
+  mod run_method {
+    use crate::applications::AppDefinition;
+    use crate::applications::ripgrep::RipGrep;
+    use crate::configuration::Version;
+    use crate::executables::RunMethod;
+    use crate::installation::{BinFolder, Method};
+    use crate::platform::{Cpu, Os, Platform};
+
+    #[test]
+    fn linux_arm() {
+      let have = (RipGrep {}).run_method(
+        &Version::from("14.1.1"),
+        Platform {
+          os: Os::Linux,
+          cpu: Cpu::Arm64,
+        },
+      );
+      let want = RunMethod::ThisApp {
+        install_methods: vec![Method::DownloadArchive {
+          url: "https://github.com/BurntSushi/ripgrep/releases/download/14.1.1/ripgrep-14.1.1-aarch64-unknown-linux-gnu.tar.gz".into(),
+          bin_folder: BinFolder::Subfolder {
+            path: "ripgrep-14.1.1-aarch64-unknown-linux-gnu".into(),
+          },
+        }],
+      };
+      assert_eq!(have, want);
+    }
+
+    #[test]
+    fn linux_intel() {
+      let have = (RipGrep {}).run_method(
+        &Version::from("14.1.1"),
+        Platform {
+          os: Os::Linux,
+          cpu: Cpu::Intel64,
+        },
+      );
+      let want = RunMethod::ThisApp {
+        install_methods: vec![Method::DownloadArchive {
+          url: "https://github.com/BurntSushi/ripgrep/releases/download/14.1.1/ripgrep-14.1.1-x86_64-unknown-linux-musl.tar.gz".into(),
+          bin_folder: BinFolder::Subfolder {
+            path: "ripgrep-14.1.1-x86_64-unknown-linux-musl".into(),
+          },
+        }],
+      };
+      assert_eq!(have, want);
+    }
+
+    #[test]
+    fn macos_arm() {
+      let have = (RipGrep {}).run_method(
+        &Version::from("14.1.1"),
+        Platform {
+          os: Os::MacOS,
+          cpu: Cpu::Arm64,
+        },
+      );
+      let want = RunMethod::ThisApp {
+        install_methods: vec![Method::DownloadArchive {
+          url: "https://github.com/BurntSushi/ripgrep/releases/download/14.1.1/ripgrep-14.1.1-aarch64-apple-darwin.tar.gz".into(),
+          bin_folder: BinFolder::Subfolder {
+            path: "ripgrep-14.1.1-aarch64-apple-darwin".into(),
+          },
+        }],
+      };
+      assert_eq!(have, want);
+    }
+
+    #[test]
+    fn macos_intel() {
+      let have = (RipGrep {}).run_method(
+        &Version::from("14.1.1"),
+        Platform {
+          os: Os::MacOS,
+          cpu: Cpu::Intel64,
+        },
+      );
+      let want = RunMethod::ThisApp {
+        install_methods: vec![Method::DownloadArchive {
+          url: "https://github.com/BurntSushi/ripgrep/releases/download/14.1.1/ripgrep-14.1.1-x86_64-apple-darwin.tar.gz".into(),
+          bin_folder: BinFolder::Subfolder {
+            path: "ripgrep-14.1.1-x86_64-apple-darwin".into(),
+          },
+        }],
+      };
+      assert_eq!(have, want);
+    }
+
+    #[test]
+    fn windows_arm() {
+      let have = (RipGrep {}).run_method(
+        &Version::from("14.1.1"),
+        Platform {
+          os: Os::Windows,
+          cpu: Cpu::Arm64,
+        },
+      );
+      let want = RunMethod::ThisApp {
+        install_methods: vec![Method::DownloadArchive {
+          url: "https://github.com/BurntSushi/ripgrep/releases/download/14.1.1/ripgrep-14.1.1-aarch64-pc-windows-msvc.zip".into(),
+          bin_folder: BinFolder::Subfolder {
+            path: "ripgrep-14.1.1-aarch64-pc-windows-msvc".into(),
+          },
+        }],
+      };
+      assert_eq!(have, want);
+    }
+
+    #[test]
+    fn windows_intel() {
+      let have = (RipGrep {}).run_method(
+        &Version::from("14.1.1"),
+        Platform {
+          os: Os::Windows,
+          cpu: Cpu::Intel64,
+        },
+      );
+      let want = RunMethod::ThisApp {
+        install_methods: vec![Method::DownloadArchive {
+          url: "https://github.com/BurntSushi/ripgrep/releases/download/14.1.1/ripgrep-14.1.1-x86_64-pc-windows-msvc.zip".into(),
+          bin_folder: BinFolder::Subfolder {
+            path: "ripgrep-14.1.1-x86_64-pc-windows-msvc".into(),
+          },
+        }],
+      };
+      assert_eq!(have, want);
+    }
+  }
+
+  #[test]
+  fn extract_version() {
+    assert_eq!(super::extract_version("ripgrep 14.1.1"), Ok("14.1.1"));
+    assert_eq!(super::extract_version("other"), Err(UserError::RegexDoesntMatch));
+  }
+}
