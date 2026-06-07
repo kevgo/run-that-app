6e18163c36c7dd7a3c2ed602feb7f370adc76501
Kevin Goslar <kevin.goslar@gmail.com>
2026-06-07 12:34:54 -0500

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 use std::process::ExitCode;
 
-use rta::applications::AppDefinition;
-
 fn main() -> ExitCode {
-  let gh = rta::applications::Gh {};
-  let name = gh.name();
   match rta::run(std::env::args().skip(1)) {
     Ok(exitcode) => exitcode,
     Err(err) => {
