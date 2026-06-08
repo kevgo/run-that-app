59e17087722aecf233d72f540411fdd732170ce4
Kevin Goslar <kevin.goslar@gmail.com>
2026-06-08 05:34:19 -0500

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,6 @@
 use std::process::ExitCode;
 
 fn main() -> ExitCode {
-  let actionlint = rta::applications::ActionLint {};
-  let apps = rta::applications::all();
-  let cmd = rta::get_cmd(
-    &actionlint,
-    rta::GetCmdArgs {
-      version: Some("1.7.12".into()),
-      app_args: vec!["--help".into()],
-      error_on_output: false,
-      from_source: false,
-      include_apps: vec![],
-      optional: false,
-      verbose: true,
-    },
-    &apps,
-  );
-  let mut cmd = cmd.unwrap().unwrap();
-  let exit_code = cmd.status().unwrap();
   match rta::run(std::env::args().skip(1)) {
     Ok(exitcode) => exitcode,
     Err(err) => {
