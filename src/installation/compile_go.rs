e00700948a642085e9192d814cb55024d6a60b2d
Kevin Goslar <kevin.goslar@gmail.com>
2026-06-16 06:38:53 -0500

    Simplify Executable implementation (#665)

--- src/installation/compile_go.rs
+++ src/installation/compile_go.rs
@@ -53,7 +53,7 @@ fn load_rta_go(optional: bool, from_source: bool, ctx: &RuntimeContext) -> Resul
     RequestedVersions::from(versions)
   };
   if let Some(executable_call) = commands::load_or_install_app(&go, &requested_go_versions, optional, from_source, ctx)? {
-    return Ok(Some(executable_call.executable.inner()));
+    return Ok(Some(executable_call.executable.into()));
   }
   Ok(None)
 }
