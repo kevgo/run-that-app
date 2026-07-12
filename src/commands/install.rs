053571bf8a0cc0ed58b2b95a4f0f340d39e3d6de
Kevin Goslar <kevin.goslar@gmail.com>
2026-07-12 15:29:53 -0500

    install.rs reinstall.rs lib.rs [skip ci]

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -7,9 +7,19 @@ use crate::yard::Yard;
 use crate::{logging, platform, yard};
 use std::process::ExitCode;
 
-pub fn install(args: &InstallArgs, apps: &Apps) -> Result<ExitCode> {
-  let app_to_install = apps.lookup(&args.app_name)?;
-  let log = logging::new(args.verbose);
+pub fn install(
+  InstallArgs {
+    app_name,
+    version,
+    from_source,
+    include_apps,
+    optional,
+    verbose,
+  }: InstallArgs,
+  apps: &Apps,
+) -> Result<ExitCode> {
+  let app_to_install = apps.lookup(&app_name)?;
+  let log = logging::new(verbose);
   let platform = platform::detect(log)?;
   let yard = Yard::load_or_create(&yard::production_location()?)?;
   let config_file = configuration::File::load(apps)?;
@@ -20,19 +30,19 @@ pub fn install(args: &InstallArgs, apps: &Apps) -> Result<ExitCode> {
     log,
   };
   // install the included apps
-  let include_apps = apps.lookup_many(&args.include_apps)?;
-  load_or_install_apps(include_apps, apps, args.optional, &ctx)?;
+  let include_apps = apps.lookup_many(&include_apps)?;
+  load_or_install_apps(include_apps, apps, optional, &ctx)?;
   // install the main app
   match load_or_install_app_and_carrier(LoadOrInstallAppAndCarrierArgs {
     app: app_to_install,
-    cli_version: args.version.as_ref(),
-    optional: args.optional,
-    from_source: args.from_source,
+    cli_version: version.as_ref(),
+    optional,
+    from_source,
     ctx: &ctx,
     apps,
   })? {
     LoadOrInstallAppOutcome::Loaded { executable_call: _ } => Ok(ExitCode::SUCCESS),
-    LoadOrInstallAppOutcome::NotInstallable { app: _ } if args.optional => Ok(ExitCode::SUCCESS),
+    LoadOrInstallAppOutcome::NotInstallable { app: _ } if optional => Ok(ExitCode::SUCCESS),
     LoadOrInstallAppOutcome::NotInstallable { app } => Err(UserError::UnsupportedPlatform { app }),
   }
 }
--- src/commands/reinstall.rs
+++ src/commands/reinstall.rs
@@ -4,7 +4,7 @@ use crate::error::Result;
 use crate::yard::{self, Yard};
 use std::process::ExitCode;
 
-pub fn reinstall(args: &install::InstallArgs, apps: &Apps) -> Result<ExitCode> {
+pub fn reinstall(args: install::InstallArgs, apps: &Apps) -> Result<ExitCode> {
   let yard = Yard::load_or_create(&yard::production_location()?)?;
   yard.delete_app_folders(&args.app_name, args.version.as_ref())?;
   install(args, apps)
