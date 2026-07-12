1c83dbbde3d35384d0b1688c668c69b7144856ee
Kevin Goslar <kevin.goslar@gmail.com>
2026-07-12 15:15:04 -0500

    mod.rs

--- src/installation/mod.rs
+++ src/installation/mod.rs
@@ -200,14 +200,15 @@ pub fn install(
   apps: &Apps,
 ) -> Result<Outcome> {
   let staging_folder = ctx.yard.create_staging_folder(&app_definition.name(), version)?;
-  match match install_method {
+  let outcome = match install_method {
     Method::DownloadArchive { url, bin_folder } => download_archive::run(app_definition, &staging_folder, version, url, bin_folder, optional, ctx),
     Method::DownloadExecutable { url: download_url } => download_executable::run(app_definition, &staging_folder, version, download_url, optional, ctx),
     Method::CompileGoSource { import_path } => compile_go::run(&staging_folder, import_path, optional, from_source, ctx, apps),
     Method::CompileRustCrate { name, bin_folder: _ } => compile_rust::run(app_definition, version, &staging_folder, &RustSource::CratesIo { name }, ctx.log),
     Method::CompileRustRepo { url } => compile_rust::run(app_definition, version, &staging_folder, &RustSource::Repository { url: url.clone() }, ctx.log),
     Method::InstallNodeJSPackage { package } => install_nodejs_package::run(package, &staging_folder, version, optional, apps),
-  }? {
+  }?;
+  match outcome {
     Outcome::Installed => {
       let app_folder_path = ctx.yard.app_folder(&app_definition.name(), version);
       ctx.yard.move_staging_folder_to_app_folder(staging_folder, app_folder_path)?;
