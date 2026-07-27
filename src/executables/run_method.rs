e53e0cb83a252b68ea27d0386bae93986f21688d
Kevin Goslar <kevin.goslar@gmail.com>
2026-07-27 07:08:59 -0500

    run_method.rs

--- src/executables/run_method.rs
+++ src/executables/run_method.rs
@@ -22,7 +22,6 @@ pub enum RunMethod {
   /// executes a shell script bundled with another app
   OtherAppShellScript {
     /// the other application that contains the shell script
-    // TODO rename this field to "carier_app" in all variants
     carrier_app: Box<dyn AppDefinition>,
     /// name of the shell script to run
     script_name: &'static str,
