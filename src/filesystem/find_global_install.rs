c75fe94a0bfaa2931860f1fb8c23bbd44bb67dd2
Kevin Goslar <kevin.goslar@gmail.com>
2026-07-28 20:32:14 -0500

    find_global_install.rs

--- src/filesystem/find_global_install.rs
+++ src/filesystem/find_global_install.rs
@@ -1,14 +1,15 @@
+use crate::applications::AppDefinition;
 use crate::executables::{Executable, ExecutableNamePlatform};
 use crate::logging::{Event, Log};
 use which::which_global;
 
-pub fn find_global_install(binary_name: &ExecutableNamePlatform, log: Log) -> Option<Executable> {
+pub fn find_global_install(app_to_install: &dyn AppDefinition, binary_name: &ExecutableNamePlatform, log: Log) -> Option<Executable> {
   log(Event::GlobalInstallSearch { binary: binary_name.as_ref() });
-  if let Ok(path) = which_global(binary_name.as_ref()) {
-    log(Event::GlobalInstallFound { path: &path });
-    Some(Executable::from(path))
-  } else {
+  let Ok(path) = which_global(binary_name.as_ref()) else {
     log(Event::GlobalInstallNotFound);
-    None
-  }
+    return None;
+  };
+  match app_to_install.run_method(version, platform)
+  log(Event::GlobalInstallFound { path: &path });
+  Some(Executable::from(path))
 }
